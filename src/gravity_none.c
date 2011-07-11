8195e023f9f256c5dc3ef9fbf0aea01a06ad5f1a
Hanno Rein <hanno@hanno-rein.de>
2011-07-11 10:07:24 +0800

    added dummy file for simulations without self-gravity

---
+++ src/gravity_none.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+#include "particle.h"
+#include "main.h"
+#include "boundaries.h"
+
+void calculate_forces(){
+	for (int i=0; i<N; i++){
+		particles[i].ax = 0; 
+		particles[i].ay = 0; 
+		particles[i].az = 0; 
+	}
+}
