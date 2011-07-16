4a4fba4da3b74aa6c71193e57b68c74fde0e6fee
Hanno Rein <hanno@hanno-rein.de>
2011-07-16 21:05:21 +0800

    Cleaning up tree.c... Every function starts with tree_ now. Changed only a few other things where I expect a small speedup.

--- src/tree.c
+++ src/tree.c
@@ -13,7 +13,10 @@ int root_nx;
 int root_ny;
 int root_nz;
 
-void add_particle_to_tree(int pt);
+void tree_add_particle_to_tree(int pt);
+int tree_get_root_for_particle(int pt);
+int tree_get_octant_for_particle_in_cell(int pt, struct cell *node);
+struct cell *tree_add_particle_to_cell(struct cell *node, int pt, struct cell *parent, int o);
 
 void tree_init(){
 	printf("Initializing the tree\n");
@@ -34,12 +37,12 @@ void tree_init(){
 	}
 
 	for (int i=0;i<N;i++){
-		add_particle_to_tree(i);
+		tree_add_particle_to_tree(i);
 	}
 }
 
 
-int get_roottree_for_particle(int pt){
+int tree_get_root_for_particle(int pt){
 	struct particle p = particles[pt];
 	int i = ((int)floor((p.x + boxsize_x/2.)/boxsize_min));
 	int j = ((int)floor((p.y + boxsize_y/2.)/boxsize_min));
@@ -48,18 +51,21 @@ int get_roottree_for_particle(int pt){
 	return index;
 }
 
-void add_particle_to_tree(int pt){
-	root[get_roottree_for_particle(pt)] = add_particle(root[get_roottree_for_particle(pt)],pt,NULL,0);
+void tree_add_particle_to_tree(int pt){
+	int root_index = tree_get_root_for_particle(pt);
+	root[root_index] = tree_add_particle_to_cell(root[root_index],pt,NULL,0);
 }
 
-struct cell *add_particle(struct cell *node, int n, struct cell *parent, int o){
+struct cell *tree_add_particle_to_cell(struct cell *node, int pt, struct cell *parent, int o){
 	if (node == NULL) {
 		node = calloc(1, sizeof(struct cell));
-		struct particle p = particles[n];
+		struct particle p = particles[pt];
+#ifdef GRAVITY_TREE
 		node->m		= p.m;
 		node->mx	= p.x;
 		node->my	= p.y;
 		node->mz	= p.z;
+#endif // GRAVITY_TREE
 		if (parent == NULL){
 			node->w = boxsize_min;
 			int i = ((int)floor((p.x + boxsize_x/2.)/boxsize_min));
@@ -77,37 +83,41 @@ struct cell *add_particle(struct cell *node, int n, struct cell *parent, int o){
 
 		// Double usages: in a leaf node, it stores the index of a particle; in a non-
 		// leaf node, it equals to (-1)*Total Number of particles within that cell.
-		node->pt = n;
-		for (int i=0; i<8; i++) node->oct[i] = NULL;
+		node->pt = pt;
+		for (int i=0; i<8; i++){
+			node->oct[i] = NULL;
+		}
 	} else if (node->pt >= 0) {
-//		node.m	+= particles[n].m;
-//		node.mx	=  (node.mx*node.m + particles[n].m*particles[n].x) / node.m;
-//		node.my	=  (node.my*node.m + particles[n].m*particles[n].y) / node.m;
-//		node.mz	=  (node.mz*node.m + particles[n].m*particles[n].z) / node.m;
-		int o;
-		o = get_octant(node->pt, node);
-		node->oct[o] = add_particle(node->oct[o], node->pt, node, o); 
-		o = get_octant(n, node);
-		node->oct[o] = add_particle(node->oct[o], n, node, o);
+#ifdef GRAVITY_TREE
+//		node.m	+= particles[pt].m;
+//		node.mx	=  (node.mx*node.m + particles[pt].m*particles[pt].x) / node.m;
+//		node.my	=  (node.my*node.m + particles[pt].m*particles[pt].y) / node.m;
+//		node.mz	=  (node.mz*node.m + particles[pt].m*particles[pt].z) / node.m;
+#endif
+		int o = tree_get_octant_for_particle_in_cell(node->pt, node);
+		node->oct[o] = tree_add_particle_to_cell(node->oct[o], node->pt, node, o); 
+		o = tree_get_octant_for_particle_in_cell(pt, node);
+		node->oct[o] = tree_add_particle_to_cell(node->oct[o], pt, node, o);
 		node->pt = -2;
 		}
 	else {
 		node->pt--;
-		int o = get_octant(n, node);
-		node->oct[o] = add_particle(node->oct[o], n, node, o);
+		int o = tree_get_octant_for_particle_in_cell(pt, node);
+		node->oct[o] = tree_add_particle_to_cell(node->oct[o], pt, node, o);
 	}
 	return node;
 }
 
-int get_octant(int n, struct cell *node){
+int tree_get_octant_for_particle_in_cell(int pt, struct cell *node){
 	int octant = 0;
-	octant = ldexp((particles[n].x > node->x) ? 0 : 1 , 0)	+ \
-			ldexp((particles[n].y > node->y) ? 0 : 1 , 1) + \
-			ldexp((particles[n].z > node->z) ? 0 : 1 , 2);
+	struct particle p = particles[pt];
+	if (p.x<node->x) octant+=1;
+	if (p.y<node->y) octant+=2;
+	if (p.z<node->z) octant+=4;
 	return octant;
 }
 
-int isInside(struct cell *node){
+int tree_particle_is_inside_cell(struct cell *node){
 	if (fabs(particles[node->pt].x-node->x) > node->w || \
 		fabs(particles[node->pt].y-node->y) > node->w || \
 		fabs(particles[node->pt].z-node->z) > node->w) {
@@ -116,14 +126,15 @@ int isInside(struct cell *node){
 	return 1;
 }
 
-struct cell *update_tree(struct cell *node){
+struct cell *tree_update_cell(struct cell *node){
 	int test;
 	if (node == NULL) {
 		return NULL;
+	}
 	// Non-leaf nodes	
-	} else	if (node->pt < 0) {
+	if (node->pt < 0) {
 		for (int o = 0; o < 8; o++) {
-			node->oct[o] = update_tree(node->oct[o]);
+			node->oct[o] = tree_update_cell(node->oct[o]);
 		}
 		// Calculate the total mass and center of mass, and check if the node needs 
 		// derefinement after updating the tree.
@@ -162,11 +173,13 @@ struct cell *update_tree(struct cell *node){
 			}
 		}
 		return node;
+	} 
 	// Leaf nodes
-	} else if (t == 0) {
+	if (t == 0) {
 		return node;
-	} else if (isInside(node) == 0) {
-		add_particle_to_tree(node->pt);
+	}
+	if (tree_particle_is_inside_cell(node) == 0) {
+		tree_add_particle_to_tree(node->pt);
 		free(node);
 		return NULL; 
 	} else {
@@ -179,7 +192,7 @@ void tree_update(){
 	for(int j=0;j<root_ny;j++){
 	for(int k=0;k<root_nz;k++){
 		int index = (k*root_ny+j)*root_nx+i;
-		root[index] = update_tree(root[index]);
+		root[index] = tree_update_cell(root[index]);
 	}
 	}
 	}
--- src/tree.h
+++ src/tree.h
@@ -3,16 +3,18 @@
 struct cell;
 
 struct cell {
+#ifdef GRAVITY_TREE
 	double m;
 	double mx;
 	double my;
 	double mz;
+#endif
 	double x;
 	double y;
 	double z;
 	double w;
 	struct cell *oct[8];
-	int pt;							// has double usages
+	int pt;						// has double usages
 } cell;
 
 extern struct cell** root;
@@ -20,17 +22,7 @@ extern int root_nx;
 extern int root_ny;
 extern int root_nz;
 
-struct cell *add_particle(struct cell *, int, struct cell *,  int);
-
-int get_octant(int n, struct cell *node);
-
-int isInside(struct cell *);
-
 void tree_init();
 void tree_update();
-
-struct cell *update_tree(struct cell *node);
-
-//void finalize_tree();
 
 #endif
