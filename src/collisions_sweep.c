de4a6b761974fce16000954c5dac915eeae10d8a
Hanno Rein <hanno@hanno-rein.de>
2011-07-18 14:16:13 +0800

    Moved collisions_resolve() to separate file. Also changed collisions_tree to work with particle index rather then particle pointer.

--- src/collisions_sweep.c
+++ src/collisions_sweep.c
@@ -5,23 +5,13 @@
 #include <time.h>
 #include "particle.h"
 #include "collisions.h"
+#include "collision_resolve.h"
 #include "main.h"
 #include "boundaries.h"
 
 
 // Dummy. No collision search
 double collisions_max_r;
-
-struct collision{
-	int p1;
-	int p2;
-	struct ghostbox gb;
-	double time;
-	int crossing;
-} collision;
-
-double coefficient_of_restitution = 1;
-double minimum_collision_velocity = 0;
 
 void collisions_resolve_single(struct collision c);
 void detect_collision_of_pair(const int pt1, const int pt2, struct ghostbox const gb, int proc, int crossing);
@@ -342,55 +332,4 @@ void collisions_resolve(){
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-void collisions_resolve_single(struct collision c){
-	struct particle p1 = particles[c.p1];
-	struct particle p2 = particles[c.p2];
-	if (p1.lastcollision==t || p2.lastcollision==t) return;
-	double m21  = p1.m  /  p2.m; 
-	double x21  = p1.x  - (p2.x+c.gb.shiftx); 
-	double y21  = p1.y  - (p2.y+c.gb.shifty); 
-	double z21  = p1.z  - (p2.z+c.gb.shiftz); 
-	double vx21 = p1.vx - (p2.vx+c.gb.shiftvx); 
-	double vy21 = p1.vy - (p2.vy+c.gb.shiftvy); 
-	double vz21 = p1.vz - (p2.vz+c.gb.shiftvz); 
-	if (vx21*x21 + vy21*y21 + vz21*z21 >0) return; // not approaching
-	// Bring the to balls in the xy plane.
-	// NOTE: this could probabely be an atan (which is faster than atan2)
-	double theta = atan2(z21,y21);
-	double stheta = sin(theta);
-	double ctheta = cos(theta);
-	double vy21n = ctheta * vy21 + stheta * vz21;	
-	double y21n = ctheta * y21 + stheta * z21;	
-	
-	// Bring the two balls onto the positive x axis.
-	double phi = atan2(y21n,x21);
-	double cphi = cos(phi);
-	double sphi = sin(phi);
-	double vx21nn = cphi * vx21  + sphi * vy21n;		
-
-	// Coefficient of restitution
-	double eps= coefficient_of_restitution;
-	double dvx2 = -(0.5+0.5*eps)*2.0*vx21nn/(1.0+m21) ;
-
-	if (dvx2<minimum_collision_velocity){
-		dvx2 = minimum_collision_velocity;
-	}
-
-	// Now we are rotating backwards
-	double dvx2n = cphi * dvx2;		
-	double dvy2n = sphi * dvx2;		
-	double dvy2nn = ctheta * dvy2n;	
-	double dvz2nn = stheta * dvy2n;	
-
-	// Applying the changes to the particles.
-	particles[c.p2].vx -=	m21*dvx2n;
-	particles[c.p2].vy -=	m21*dvy2nn;
-	particles[c.p2].vz -=	m21*dvz2nn;
-	particles[c.p2].lastcollision = t;
-	particles[c.p1].vx +=	dvx2n; 
-	particles[c.p1].vy +=	dvy2nn; 
-	particles[c.p1].vz +=	dvz2nn; 
-	particles[c.p1].lastcollision = t;
-
-}
 
