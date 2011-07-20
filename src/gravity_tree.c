6590d346f20df3092e4f7bb32bdcf7d0914db98d
Shangfei Liu <shangfei.liu@gmail.com>
2011-07-20 17:19:35 +0800

    Add Quadrupole component of mass distribution in gravity calculation.

--- src/gravity_tree.c
+++ src/gravity_tree.c
@@ -7,11 +7,15 @@
 #include "main.h"
 #include "tree.h"
 #include "boundaries.h"
+#ifndef QUADRUPOLE
+	#define QUADRUPOLE
+#endif
 
 double opening_angle2 = 0.25;
 
 void calculate_forces_for_particle(int pt, struct ghostbox gb);
 void calculate_forces_for_particle_from_cell(int pt, struct cell *node, struct ghostbox gb);
+void calculate_quadrupole_tensor(double *mqp, struct cell *node, double mcx, double mcy, double mcz); 
 
 void calculate_forces(){
 	tree_update();
@@ -66,5 +70,63 @@ void calculate_forces_for_particle_from_cell(int pt, struct cell *node, struct g
 		particles[pt].ax += prefact*dx; 
 		particles[pt].ay += prefact*dy; 
 		particles[pt].az += prefact*dz; 
+#ifdef QUADRUPOLE
+		if (node->pt < 0) {
+			double mq[3][3];
+			for (int i=0; i<9; i++) {
+				mq[i/3][i%3] = 0;
+			}
+			calculate_quadrupole_tensor(&mq[0][0], node, node->mx, node->my, node->mz);
+			double m4 = dx*dx*mq[0][0] + \
+						dx*dy*mq[0][1] + \
+						dx*dz*mq[0][2] + \
+						dy*dx*mq[1][0] + \
+						dy*dy*mq[1][1] + \
+						dy*dz*mq[1][2] + \
+						dz*dx*mq[2][0] + \
+						dz*dy*mq[2][1] + \
+						dz*dz*mq[2][2]; 
+			double qprefact = -G/(2*r*r*r*r*r)*m4;
+			particles[pt].ax += qprefact*dx; 
+			particles[pt].ay += qprefact*dy; 
+			particles[pt].az += qprefact*dz; 
+		}
+#endif
 	}
 }
+
+#ifdef QUADRUPOLE
+void calculate_quadrupole_tensor(double *mqp, struct cell *node, double mcx, double mcy, double mcz) {
+	for (int o=0; o<8; o++) {
+		if (node->oct[o] != NULL) {
+			if (node->oct[o]->pt >= 0) {
+				double pm	= node->oct[o]->m;
+				double px	= node->oct[o]->mx - mcx;
+				double py	= node->oct[o]->my - mcy;
+				double pz	= node->oct[o]->mz - mcz;
+				double pr2	= px*px + py*py + pz*pz;
+				*(mqp)   += pm * (3*px*px - pr2);
+				*(mqp+1)   += pm * 3*px*py; 
+				*(mqp+2)   += pm * 3*px*pz; 
+				*(mqp+3)   += pm * 3*py*px; 
+				*(mqp+4)   += pm * (3*py*py - pr2);
+				*(mqp+5)   += pm * 3*py*pz; 
+				*(mqp+6)   += pm * 3*pz*px; 
+				*(mqp+7)   += pm * 3*pz*py; 
+				*(mqp+8)   += pm * (3*pz*pz - pr2);
+			/*	mq[0][0]   += pm * (3*px*px - pr2);
+				mq[0][1]   += pm * 3*px*py; 
+				mq[0][2]   += pm * 3*px*pz; 
+				mq[1][0]   += pm * 3*py*px; 
+				mq[1][1]   += pm * (3*py*py - pr2);
+				mq[1][2]   += pm * 3*py*pz; 
+				mq[2][0]   += pm * 3*pz*px; 
+				mq[2][1]   += pm * 3*pz*py; 
+				mq[2][2]   += pm * (3*pz*pz - pr2); */
+			} else {
+				calculate_quadrupole_tensor(mqp, node->oct[o], mcx, mcy, mcz);
+			}
+		}
+	}
+}
+#endif
