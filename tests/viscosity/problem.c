b89de8f9a1917141370de17bfa93e1d411648e97
Hanno Rein <hanno@hanno-rein.de>
2011-07-20 15:18:47 +0800

    Viscosity calculation seems ok. Needs to be testes at better resolution.

--- tests/viscosity/problem.c
+++ tests/viscosity/problem.c
@@ -13,32 +13,36 @@ extern double OMEGA;
 extern double coefficient_of_restitution;
 extern double minimum_collision_velocity;
 double particle_radius = 1;
+double r_hstar 		= 0.5;
 
 void problem_init(int argc, char* argv[]){
 	// Setup constants
-	OMEGA = 1.;
-	G=1;
+	OMEGA 	= 1.;
+	G	= 1;
 	// Setup particle structures
-	double r_hstar = 0.5;
-	double r_h = r_hstar*2.*particle_radius;
-	double particle_mass = r_h*r_h*r_h*3./2.;
-	double tau = 0.5;
-	double surface_density = tau/(M_PI*particle_radius*particle_radius)*particle_mass;
-	double lambda_crit = 4.*M_PI*M_PI*G*surface_density/OMEGA/OMEGA;
-	boxsize = 50;
-	N = (int)round(tau*boxsize*boxsize/(M_PI*particle_radius*particle_radius));
-	printf("lambda_crit = %f\nsurface_density = %f\nboxsize = %f\n",lambda_crit,surface_density,boxsize);
-	init_particles(N);
+	if (argc>1){
+		r_hstar = atof(argv[1]);
+	}
+	double r_h 		= r_hstar*2.*particle_radius;
+	double particle_mass 	= r_h*r_h*r_h*3./2.;
+	double tau 		= 0.5;
+	double surface_density 	= tau/(M_PI*particle_radius*particle_radius)*particle_mass;
+	double lambda_crit 	= 4.*M_PI*M_PI*G*surface_density/OMEGA/OMEGA;
+	boxsize 		= 20;
+	N 			= (int)round(tau*boxsize*boxsize/(M_PI*particle_radius*particle_radius));
 	coefficient_of_restitution = 0.5;
 	minimum_collision_velocity = 0.01*OMEGA*particle_radius;
-	dt = 1e-2;
-	//tmax=10.*2.*M_PI;
-	softening = 0.1*particle_radius;
+	dt 			= 1e-2;
+	tmax			= 20.*2.*M_PI;
+	softening 		= 0.1*particle_radius;
+	// Nothing needs to be changes below.
+	init_particles(N);
+	printf("lambda_crit = %f\nsurface_density = %f\nboxsize = %f\nparticle_mass = %f\n",lambda_crit,surface_density,boxsize,particle_mass);
 	// Initial conditions
 	for (int i =0;i<N;i++){
 		particles[i].x = ((double)rand()/(double)RAND_MAX-0.5)*boxsize_x;
 		particles[i].y = ((double)rand()/(double)RAND_MAX-0.5)*boxsize_y;
-		particles[i].z = 2.*particle_radius*((double)rand()/(double)RAND_MAX-0.5);
+		particles[i].z = 3.*particle_radius*((double)rand()/(double)RAND_MAX-0.5);
 		particles[i].vx = 0;
 		particles[i].vy = -1.5*particles[i].x*OMEGA;
 		particles[i].vz = 0;
@@ -69,10 +73,24 @@ void calculate_viscosity(double* nutrans, double* nucoll, double* nugrav){
 		mtot += p.m;
 		double vyr = p.vy + 1.5 * OMEGA * p.x;
 		_nutrans += p.m*p.vx*vyr;
+		for (int j=0;j<N;j++){
+			struct particle p2 = particles[j];
+			if (p2.x<=p.x) continue;
+			for (int gbx=0; gbx<=nghostx; gbx++){
+				for (int gby=-nghosty; gby<=nghosty; gby++){
+					struct ghostbox gb = get_ghostbox(gbx,gby,0);
+					double dx = p2.x+gb.shiftx - p.x;
+					double dy = p2.y+gb.shifty - p.y;
+					double dz = p2.z - p.z;
+					double r2 = dx*dx + dy*dy + dz*dz + softening*softening;
+					_nugrav += p.m*p2.m*dy*dx/pow(r2,3./2.);
+				}
+			}
+		}
 	}
-	*nutrans = _nutrans*2./(3.*OMEGA*mtot);
-	*nucoll  = _nutrans*2./(3.*OMEGA*mtot)*1./(t-collisions_plog_last)*collisions_plog;
-	*nugrav  = _nugrav;
+	*nutrans = 2./(3.*OMEGA*mtot)*_nutrans;
+	*nugrav  = 2./(3.*OMEGA*mtot)*_nugrav * (-G);
+	*nucoll  = 2./(3.*OMEGA*mtot)*1./(t-collisions_plog_last)*collisions_plog;
 }
 
 double nutrans_tot=0, nucoll_tot=0, nugrav_tot=0; int nuN=0;
@@ -87,21 +105,16 @@ void calculate_viscosity_tot(){
 	nuN++;
 }
 
-void output_append_viscosity(char* filename){
-	FILE* of = fopen(filename,"a"); 
-	fprintf(of,"%e\t%e\t%e\t%e\n",t,nutrans_tot/(particle_radius*particle_radius*OMEGA)/(double)nuN,nucoll_tot/(particle_radius*particle_radius*OMEGA)/(double)nuN,nugrav_tot/(particle_radius*particle_radius*OMEGA)/(double)nuN);
-	fclose(of);
-}
 
 void problem_output(){
 	if (output_check(1e-1*2.*M_PI/OMEGA)&&t>0){
 		calculate_viscosity_tot();
-		output_append_viscosity("viscosity.txt");
 	}
 }
 
 
 void problem_finish(){
+	calculate_viscosity_tot();
 	FILE* of = fopen("error.txt","a+"); 
 	double error= 0;
 	struct timeval tim;
@@ -113,5 +126,9 @@ void problem_finish(){
 	fprintf(of,"N_init = %d",0);
 	fprintf(of,"\n");
 	fclose(of);
+	
+	FILE* ofv = fopen("viscosity.txt","a"); 
+	fprintf(ofv,"%e\t%e\t%e\t%e\t%e\n",t,r_hstar,nutrans_tot/(particle_radius*particle_radius*OMEGA)/(double)nuN,nucoll_tot/(particle_radius*particle_radius*OMEGA)/(double)nuN,nugrav_tot/(particle_radius*particle_radius*OMEGA)/(double)nuN);
+	fclose(ofv);
 
 }
