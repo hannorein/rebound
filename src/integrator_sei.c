5c648af6e4767e31f08f60e0cc87fd35957f82ca
Hanno Rein <hanno@hanno-rein.de>
2011-07-27 14:47:43 +0800

    Simplified SEI integrator.

--- src/integrator_sei.c
+++ src/integrator_sei.c
@@ -11,8 +11,8 @@
 double OMEGA = 1.; // Epicyclic frequency 
 double OMEGAZ = -1.; // Epicyclic frequency in vertical direction
 
-void operator_H0(double dt, struct particle* p);
-void operator_phi(double dt, struct particle* p);
+void operator_H012(struct particle* p);
+void operator_phi1(struct particle* p);
 // Cache sin() tan() values.
 double lastdt=0;
 double sindt, tandt;
@@ -36,7 +36,7 @@ void integrator_part1(){
 
 #pragma omp parallel for
 	for (int i=0;i<N;i++){
-		operator_H0(dt/2.,&(particles[i]));
+		operator_H012(&(particles[i]));
 	}
 	t+=dt/2.;
 }
@@ -44,15 +44,15 @@ void integrator_part1(){
 void integrator_part2(){
 #pragma omp parallel for
 	for (int i=0;i<N;i++){
-		operator_phi(dt,&(particles[i]));
-		operator_H0(dt/2.,&(particles[i]));
+		operator_phi1(&(particles[i]));
+		operator_H012(&(particles[i]));
 	}
 	t+=dt/2.;
 }
 
 // This function evolves a particle under 
 // Hamiltonian H0 exactly up to machine precission.
-void operator_H0(double _dt, struct particle* p){
+void operator_H012(struct particle* p){
 		
 	// Integrate vertical motion
 	const double zx = p->z * OMEGAZ;
@@ -80,18 +80,18 @@ void operator_H0(double _dt, struct particle* p){
 	const double xst  =  xst1 - tandt*yst;	
 
 	p->x  = (xst+aO)    /OMEGA;			
-	p->y  = (yst*2.+bO) /OMEGA - 3./2.*aO*_dt;	
+	p->y  = (yst*2.+bO) /OMEGA - 3./4.*aO*dt;	
 	p->vx = yst;
 	p->vy = -xst*2. -3./2.*aO;
 }
 
 // This function evolves a particle under the operator
 // Phi exactly up to machine precission.
-void operator_phi(double _dt, struct particle* p){
+void operator_phi1(struct particle* p){
 	// The force used here is for test cases 2 and 3 
 	// in Rein & Tremaine 2011. 
-	p->vx += p->ax * _dt;
-	p->vy += p->ay * _dt;
-	p->vz += p->az * _dt;
+	p->vx += p->ax * dt;
+	p->vy += p->ay * dt;
+	p->vz += p->az * dt;
 }
 
