a10115568e2fb3be321369b8aee80546bff2ed74
Hanno Rein <hanno@hanno-rein.de>
2011-07-28 14:07:04 +0800

    Changed documentation_mpi.h

--- src/communication_mpi.h
+++ src/communication_mpi.h
@@ -1,14 +1,42 @@
-#ifndef _COMMUNICATION_MPI_H
-#define _COMMUNICATION_MPI_H
 /**
  * @file communication_mpi.h
+ * @brief  Handles communication between nodes using MPI.
  * @author Hanno Rein <hanno@hanno-rein.de>
  *
- *
- * The functions in this file handle the communication between 
- * different nodes using the Message Passing Interface (MPI).
+ * @details These routines handle the communication between
+ * different nodes via the Message Passing Interface (MPI). 
+ * There are two different types of communications
+ * implemented at the moment:
+ * - Distributing particles to the correct node.
+ * - Creating, and distributing the essential tree to allow
+ *   other nodes walk remote trees. Note that the opening 
+ *   criteria is different for gravity and collision 
+ *   tree walks.
  * 
+ * 
+ * @section LICENSE
+ * Copyright (c) 2011 Hanno Rein, Shangfei Liu
+ *
+ * This file is part of nbody.
+ *
+ * nbody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * nbody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with nbody.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
+
+#ifndef _COMMUNICATION_MPI_H
+#define _COMMUNICATION_MPI_H
 #ifdef MPI
 #include "mpi.h"
 
