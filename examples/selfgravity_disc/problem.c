323f670d3f1dc4c3da3fd5f54a52c8ce7bc51b37
Hanno Rein <hanno@hanno-rein.de>
2011-07-30 21:45:14 +0800

    Tweaked name and initial conditions for selfgravity_disc. Now using a constant Q.

--- examples/selfgravity_disc/problem.c
+++ examples/selfgravity_disc/problem.c
@@ -10,8 +10,11 @@
 #include "output.h"
 #include "communication_mpi.h"
 #include "tree.h"
+#include "tools.h"
 
 extern double opening_angle2;
+extern int WH_SELFGRAVITY_ENABLED;
+extern int Nmax;
 
 void problem_init(int argc, char* argv[]){
 	// Setup constants
@@ -24,32 +27,42 @@ void problem_init(int argc, char* argv[]){
 	nghostx = 0; nghosty = 0; nghostz = 0; 		
 	init_box();
 	// Setup particles
-	double surfacedensity 	= 0.06; 
-	double particle_mass 	= 5e-6;
-	int _N = round(surfacedensity*M_PI*(boxsize*boxsize/4./sqrt(1.2)-boxsize*boxsize/400.)/particle_mass);
+	double disc_mass = 2e-1;
+	int _N = 10000;
 	// Initial conditions
 	struct particle star;
 	star.x 		= 0; star.y 	= 0; star.z	= 0;
 	star.vx 	= 0; star.vy 	= 0; star.vz 	= 0;
 	star.ax 	= 0; star.ay 	= 0; star.az 	= 0;
 	star.m 		= 1;
+#ifdef INTEGRATOR_WH
+	// Insert particle manually. Don't add it to tree.
+	WH_SELFGRAVITY_ENABLED 	= 1;
+	N_active_first 		= 1;
+	Nmax 			+= 128;
+	particles 		= realloc(particles,sizeof(struct particle)*Nmax);
+	particles[N] 		= star;
+	N++;
+#else // INTEGRATOR_WH
 	particles_add(star);
+#endif // INTEGRATOR_WH
 	while(N<_N){
 		struct particle pt;
-		pt.x 		= ((double)rand()/(double)RAND_MAX-0.5)*boxsize_x;
-		pt.y 		= ((double)rand()/(double)RAND_MAX-0.5)*boxsize_y;
-		pt.z 		= ((double)rand()/(double)RAND_MAX-0.5)*boxsize_z*0.1;
-		double a	 = sqrt(pt.x*pt.x + pt.y*pt.y);
-		if (a>boxsize/2./1.2 || a<boxsize/20.) continue;
-		double phi 	= atan2(pt.y,pt.x);
-		double vkep 	= sqrt(G*star.m/(a));
+		double a	= tools_powerlaw(boxsize/10.,boxsize/2./1.2,-1.5);
+		double phi 	= tools_uniform(0,2.*M_PI);
+		pt.x 		= a*cos(phi);
+		pt.y 		= a*sin(phi);
+		pt.z 		= a*tools_normal(0.001);
+		double mu 	= star.m + disc_mass * (pow(a,-3./2.)-pow(boxsize/10.,-3./2.))/(pow(boxsize/2./1.2,-3./2.)-pow(boxsize/10.,-3./2.));
+		printf("a = %f\tmu = %f\n",a,mu);
+		double vkep 	= sqrt(G*mu/a);
 		pt.vx 		=  vkep * sin(phi);
 		pt.vy 		= -vkep * cos(phi);
 		pt.vz 		= 0;
 		pt.ax 		= 0;
 		pt.ay 		= 0;
 		pt.az 		= 0;
-		pt.m 		= particle_mass;
+		pt.m 		= disc_mass/(double)_N;
 		particles_add(pt);
 	}
 }
