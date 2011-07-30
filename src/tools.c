7ab4834aed2268e7bd7796f42f3abd79de74bab3
Hanno Rein <hanno@hanno-rein.de>
2011-07-30 19:38:07 +0800

    added tools.c file for different small functions, eg. for distributions

---
+++ src/tools.c
@@ -0,0 +1,60 @@
+/**
+ * @file 	tools.c
+ * @brief 	Tools for creating distributions.
+ * @author 	Hanno Rein <hanno@hanno-rein.de>
+ * 
+ * @section 	LICENSE
+ * Copyright (c) 2011 Hanno Rein, Shangfei Liu
+ *
+ * This file is part of nbody.
+ *
+ * nbody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * nbody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with nbody.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+
+double	tools_normaldistribution2_rsq;		/**< Used for speedup**/ 
+double 	tools_normaldistribution2_v2;		/**< Used for speedup**/
+int 	tools_normaldistribution2_ready = 0;	/**< Used for speedup**/
+
+double tools_uniform(double min, double max){
+	return ((double)rand())/((double)(RAND_MAX))*(max-min)+min;
+}
+
+
+double tools_powerlaw(double min, double max, double slope){
+	double y = tools_uniform(0., 1.);
+	return pow( (pow(max,slope+1.)-pow(min,slope+1.))*y+pow(min,slope+1.), 1./(slope+1.));
+}
+
+double tools_normal(double variance){
+	if (tools_normaldistribution2_ready==1){
+		tools_normaldistribution2_ready=0;
+		return tools_normaldistribution2_v2*sqrt(-2.*log(tools_normaldistribution2_rsq)/tools_normaldistribution2_rsq*variance);
+	}
+	double v1,v2,rsq=1.;
+	while(rsq>=1. || rsq<1.0e-12){
+		v1=2.*((double)rand())/((double)(RAND_MAX))-1.0;
+		v2=2.*((double)rand())/((double)(RAND_MAX))-1.0;
+		rsq=v1*v1+v2*v2;
+	}
+	tools_normaldistribution2_ready = 1;
+	tools_normaldistribution2_rsq   = rsq;
+	tools_normaldistribution2_v2	  = v2;
+	return 	v1*sqrt(-2.*log(rsq)/rsq*variance);
+}
---
+++ src/tools.h
@@ -0,0 +1,47 @@
+/**
+ * @file 	tools.h
+ * @brief 	Tools for creating distributions.
+ * @author 	Hanno Rein <hanno@hanno-rein.de>
+ * 
+ * @section 	LICENSE
+ * Copyright (c) 2011 Hanno Rein, Shangfei Liu
+ *
+ * This file is part of nbody.
+ *
+ * nbody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * nbody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with nbody.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * Calculates a random variable in a given range.
+ * @param min Minimum value.
+ * @param max Maximum value.
+ */
+double tools_uniform(double min, double max);
+
+/**
+ * Calculates a random variable drawn form a powerlaw distribution.
+ * @param min Minimum value.
+ * @param max Maximum value.
+ * @param slop Slope of powerlaw distribution.
+ */
+double tools_powerlaw(double min, double max, double slope);
+
+/**
+ * Calculate a random number with normal distribution.
+ * Algorithm by D.E. Knut, 1997, The Art of Computer Programmin, Addison-Wesley. 
+ * @param variance Variance of normal distribution.
+ * @return Random number with normal distribution (mean 0). 
+ */
+double tools_normal(double variance);
