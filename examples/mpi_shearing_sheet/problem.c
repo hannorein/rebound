10a79b82e7cf52e4888b7d40a6bb5fc8b6c34bbb
Hanno Rein <hanno@hanno-rein.de>
2011-07-30 19:44:02 +0800

    mpi_shearing_sheet example using size distribution

--- examples/mpi_shearing_sheet/problem.c
+++ examples/mpi_shearing_sheet/problem.c
@@ -10,6 +10,7 @@
 #include "output.h"
 #include "communication_mpi.h"
 #include "tree.h"
+#include "tools.h"
 
 extern double OMEGA;
 extern double coefficient_of_restitution;
@@ -33,8 +34,9 @@ void problem_init(int argc, char* argv[]){
 	nghostx = 2; nghosty = 2; nghostz = 0; 		// Use three ghost rings
 	double surfacedensity 	= 400; 			// kg/m^2
 	double particle_density	= 400;			// kg/m^3
-	double particle_radius 	= 1;			// m
-	double particle_mass 	= particle_density*4./3.*M_PI*pow(particle_radius,3); 	// kg
+	double particle_radius_min = 1;			// m
+	double particle_radius_max = 4;			// m
+	double particle_radius_slope = -3;	
 	if (argc>1){					// Try to read boxsize from command line
 		boxsize = atof(argv[1]);
 	}else{
@@ -43,31 +45,35 @@ void problem_init(int argc, char* argv[]){
 	printf("Toomre wavelength: %f\n",2.*M_PI*M_PI*surfacedensity/OMEGA/OMEGA*G);
 	// Use Bridges et al coefficient of restitution.
 	coefficient_of_restitution_for_velocity = coefficient_of_restitution_bridges;
-	minimum_collision_velocity = particle_radius*OMEGA*0.001;  // small fraction of the shear
+	minimum_collision_velocity = particle_radius_min*OMEGA*0.001;  // small fraction of the shear
 	// Setup particle structures
 	init_box();
-	int _N = round(surfacedensity*boxsize_x*boxsize_y/particle_mass);
+	double total_mass = surfacedensity*boxsize_x*boxsize_y;
 	// Initial conditions
 #ifdef MPI
 	// Only initialise particles on master. This should also be parallised, but obviously depends on the problem.
 	if (mpi_id==0){
 #endif
-		for (int i =0;i<_N;i++){
+		double mass = 0;
+		while(mass<total_mass){
 			struct particle pt;
-			pt.x 		= ((double)rand()/(double)RAND_MAX-0.5)*boxsize_x;
-			pt.y 		= ((double)rand()/(double)RAND_MAX-0.5)*boxsize_y;
-			pt.z 		= particle_radius*((double)rand()/(double)RAND_MAX-0.5);
+			pt.x 		= tools_uniform(-boxsize_x/2.,boxsize_x/2.);
+			pt.y 		= tools_uniform(-boxsize_y/2.,boxsize_y/2.);
+			pt.z 		= tools_normal(1.);					// m
 			pt.vx 		= 0;
 			pt.vy 		= -1.5*pt.x*OMEGA;
 			pt.vz 		= 0;
 			pt.ax 		= 0;
 			pt.ay 		= 0;
 			pt.az 		= 0;
-			pt.m 		= particle_mass;
+			double radius 	= tools_powerlaw(particle_radius_min,particle_radius_max,particle_radius_slope);
 #ifndef COLLISIONS_NONE
-			pt.r 		= particle_radius;
+			pt.r 		= radius;						// m
 #endif
+			double		particle_mass = particle_density*4./3.*M_PI*radius*radius*radius;
+			pt.m 		= particle_mass; 	// kg
 			particles_add(pt);
+			mass += particle_mass;
 		}
 #ifdef MPI
 	printf("%d particles initialized on root node.\n",_N);
