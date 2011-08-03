8ea54be915c574af553a1d87dc19940d4f2da6e6
Hanno Rein <hanno@hanno-rein.de>
2011-08-03 09:14:32 +0800

    fixed os example

--- examples/overstability/problem.c
+++ examples/overstability/problem.c
@@ -16,36 +16,34 @@ extern double minimum_collision_velocity;
 
 void problem_init(int argc, char* argv[]){
 	// Setup constants
-	OMEGA = 1.;
-	OMEGAZ = 3.6;
-	// Setup particle structures
-	boxsize_x = 200;
-	boxsize_y = 4;
-	boxsize_z = 8;
-	init_particles(600);
-	dt = 1e-2*2.*M_PI;
-	double particle_size = 0.5;
-	coefficient_of_restitution = 0.5;
-	minimum_collision_velocity = 0.09*particle_size*OMEGA;
-	collisions_max_r = particle_size;
+	OMEGA 				= 1.;
+	OMEGAZ 				= 3.6;
+	dt				= 1e-2*2.*M_PI/OMEGA;
+	double particle_r 		= 1;
+	double tau			= 1;
+	coefficient_of_restitution 	= 0.5;
+//	minimum_collision_velocity 	= 0.001*particle_r*OMEGA;
+	boxsize 			= 8;
+	root_nx = 50; 	root_ny = 1; 	root_nz = 2;
+	nghostx = 1; 	nghosty = 1; 	nghostz = 0;
+	init_box();
 	// Initial conditions
-	for (int i =0;i<N;i++){
-		particles[i].x = ((double)rand()/(double)RAND_MAX-0.5)*boxsize_x;
-		particles[i].y = ((double)rand()/(double)RAND_MAX-0.5)*boxsize_y;
-		particles[i].z = 4.0*((double)rand()/(double)RAND_MAX-0.5)*particle_size;
-		particles[i].vx = 0;
-		particles[i].vy = -1.5*particles[i].x*OMEGA;
-		particles[i].vz = 0;
-		particles[i].ax = 0;
-		particles[i].ay = 0;
-		particles[i].az = 0;
-		particles[i].m = 0.0001;
-		particles[i].r = particle_size;
+	double _N = tau * boxsize_x * boxsize_y/(M_PI*particle_r *particle_r);
+	while (N<_N){
+		struct particle p;
+		p.x 	= ((double)rand()/(double)RAND_MAX-0.5)*boxsize_x;
+		p.y 	= ((double)rand()/(double)RAND_MAX-0.5)*boxsize_y;
+		p.z 	= 4.0*((double)rand()/(double)RAND_MAX-0.5)*particle_r;
+		p.vx 	= 0;
+		p.vy 	= -1.5*p.x*OMEGA;
+		p.vz 	= 0;
+		p.ax 	= 0;
+		p.ay 	= 0;
+		p.az 	= 0;
+		p.m 	= 0.0001;
+		p.r 	= particle_r;
+		particles_add(p);
 	}
-	// Do use ghost boxes in x and y
-	nghostx = 1;
-	nghosty = 1;
-	nghostz = 0;
 }
 
 void problem_inloop(){
@@ -53,7 +51,7 @@ void problem_inloop(){
 }
 
 void problem_output(){
-	if (output_check(1e1*2.*M_PI)){
+	if (output_check(dt)){
 		output_timing();
 	}
 }
