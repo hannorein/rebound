058fa8575ced2c350f664369649055f60e90f8b0
Hanno Rein <hanno@hanno-rein.de>
2012-04-20 23:41:15 +0900

    opt submit working

--- problems/wakes/submit.opt.bash
+++ problems/wakes/submit.opt.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 #PBS -N wakes
-#PBS -l nodes=22:ppn=12
-#PBS -q t-A
+#PBS -l nodes=1:ppn=6,walltime=48:00:00
+#PBS -q n-A
+
+MPI=/usr/local/mvapich2/mvapich2.1.8rc1-gcc
+PATH=${MPI}/bin:${PATH}
+LD_LIBRARY_PATH=${MPI}/lib
 
 cd ~/rebound/problems/wakes
-mpiexec -np 264 ./nbody 3 >>log.txt
+mpirun ./nbody --boxsize=10 --root_nx=6 --root_ny=6
+
