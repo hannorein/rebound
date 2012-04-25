fc54530966fb1e136a8f1ee31055571506aacf96
Hanno Rein <hanno@hanno-rein.de>
2012-04-25 11:31:18 -0400

    minor

--- src/zpr.c
+++ src/zpr.c
@@ -1,11 +1,37 @@
+/**
+ * @file 	zpr.c
+ * @brief 	Zoom-Pan-Rotate mouse manipulation module for GLUT.
+ * @author 	Nigel Stewart
+ * 		School of Computer Science and Information Technology
+ * 		RMIT University
+ * 		nigels@cs.rmit.edu.au
+ * 
+ * @section 	LICENSE
+ * Copyright (c) 2003,2012 Nigel Stewart, Hanno Rein
+ *
+ * This file is part of rebound.
+ *
+ * rebound is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rebound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <memory.h>
 #include <math.h>
 
 #include "zpr.h"
-
-/* This code was originally C++ :-) */
 
 #define bool int
 #define true 1
