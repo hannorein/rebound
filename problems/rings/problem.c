8093c88f20afc281bee49470f15b46ba8f9e5e6e
Hanno Rein <hanno@hanno-rein.de>
2012-04-30 09:47:20 -0400

    rings

--- problems/rings/problem.c
+++ problems/rings/problem.c
@@ -36,62 +36,45 @@
 #include "boundaries.h"
 
 void problem_init(int argc, char* argv[]){
-  // Setup constants
-  dt 		= M_PI*1e-3*1.1234125235345;
-  boxsize 	= 2.8;
-  N_active      = 2;
-  init_box();
+	// Setup constants
+	dt 		= M_PI*1e-3*1.1234125235345;
+	boxsize 	= 2.8;
+	N_active      = 2;
+	init_box();
 
-  // Initial conditions
-  // The WH integrator assumes a heliocentric coordinate system.
-  // Therefore the star has to be at the origin.
-  struct particle star;
-  star.x  = 0; star.y  = 0; star.z  = 0;
-  star.vx = 0; star.vy = 0; star.vz = 0;
-  star.ax = 0; star.ay = 0; star.az = 0;
-  star.m  = 1;
-  particles_add(star);
+	// Initial conditions
+	// The WH integrator assumes a heliocentric coordinate system.
+	// Therefore the star has to be at the origin.
+	struct particle star;
+	star.x  = 0; star.y  = 0; star.z  = 0;
+	star.vx = 0; star.vy = 0; star.vz = 0;
+	star.ax = 0; star.ay = 0; star.az = 0;
+	star.m  = 1;
+	particles_add(star);
 
-  // Planet 1 at 1 AU
-  struct particle planet;
-  planet.x  = 1.; planet.y  = 0.; planet.z  = 0.;
-  double r_init1 = sqrt(planet.x*planet.x + planet.y*planet.y + planet.z*planet.z);
-  planet.vx = 0;
-  planet.vy = sqrt(1./r_init1);
-  planet.vz = 0;
-  /**/
-  planet.ax = 0; planet.ay = 0; planet.az = 0;
-  planet.m  = 1.e-3;
-  particles_add(planet);
+	// Planet 1 at 1 AU
+	struct particle planet;
+	planet.m  = 1.e-3;
+	planet.x  = 1.; planet.y  = 0.; planet.z  = 0.;
+	planet.ax = 0; planet.ay = 0; planet.az = 0;
+	planet.vx = 0;
+	planet.vy = sqrt(G*(star.m+planet.m)/planet.x);
+	planet.vz = 0;
+	particles_add(planet);
 
-  // Ring particles
-  double inner_R =  1./20.;
-  double outer_R =  2./20.;
-  int NumParticles = 5000;
-  for (int ii = 1; ii <= NumParticles; ++ii) {
-    double dr = 0.;
-    if (NumParticles == 1)
-      dr = (inner_R + outer_R)/2.;
-    else
-      dr = inner_R + ((ii-1.)/(NumParticles-1)) * (outer_R-inner_R);
-    double vr = sqrt(planet.m / dr);
-    struct particle ringparticle;
-    ringparticle.x  = planet.x + dr;
-    ringparticle.y  = 0;
-    ringparticle.z  = 0;
-    double r_init2 = sqrt(ringparticle.x*ringparticle.x + \
-			  ringparticle.y*ringparticle.y + \
-			  ringparticle.z*ringparticle.z);
-    ringparticle.vx = 0;
-    ringparticle.vy = sqrt(1./r_init2);
-    ringparticle.vz = 0;
-    ringparticle.vy = ringparticle.vy + vr;
-    ringparticle.ax = 0;
-    ringparticle.ay = 0;
-    ringparticle.az = 0;
-    ringparticle.m  = 0;
-    particles_add(ringparticle);
-  }
+	// Ring particles
+	double inner_R =  1./40.;
+	double outer_R =  2./40.;
+	int _N = 5000;
+	for (int i = 0; i < _N; i++) {
+		double dr = inner_R + (double)i/(double)(_N-1) * (outer_R-inner_R);
+		double dvr = sqrt(planet.m / dr);
+		struct particle ringparticle = planet;
+		ringparticle.x  += dr;
+		ringparticle.vy += dvr;
+		ringparticle.m  = 0;
+		particles_add(ringparticle);
+	}
 }
 
 void problem_inloop(){
