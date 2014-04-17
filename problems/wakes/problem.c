3c11eeaba2e76ddc4e610f1d0611b8d52ff9e0d6
Hanno Rein <hanno@hanno-rein.de>
2014-04-17 13:01:20 -0400

    ray tracing (no mor MPI)

--- problems/wakes/problem.c
+++ problems/wakes/problem.c
@@ -263,7 +263,7 @@ void output_ascii_mod(char* filename){
 	
 double sun_b[3];
 
-double  tree_does_ray_hit_particle_above(struct cell* c, double a[3], double b[3]){
+double  tree_does_ray_hit_particle_above(struct cell* c, double a[3], double b[3],int pr){
 	double t1 = b[1]*(a[2]-c->z)-b[2]*(a[1]-c->y);
 	double t2 = b[2]*(a[0]-c->x)-b[0]*(a[2]-c->z);
 	double t3 = b[0]*(a[1]-c->y)-b[1]*(a[0]-c->x);
@@ -275,7 +275,7 @@ double  tree_does_ray_hit_particle_above(struct cell* c, double a[3], double b[3
 			for(int i=0;i<8;i++){
 				struct cell* o = c->oct[i];
 				if(o){
-					double F = tree_does_ray_hit_particle_above(o,a,b);
+					double F = tree_does_ray_hit_particle_above(o,a,b,pr);
 					if (F>0) return F;
 				}
 			}
@@ -289,7 +289,7 @@ double  tree_does_ray_hit_particle_above(struct cell* c, double a[3], double b[3
 				//double cx = a[0] + d*b[0];	// intersection point 
 				//double cy = a[1] + d*b[1];
 				double cz = a[2] + d*b[2];
-				if (cz>a[2]){
+				if (cz>a[2] && c->pt != pr){
 					return 1; 	// ray hits other sphere above scattering sphere
 				}
 			}
@@ -298,7 +298,7 @@ double  tree_does_ray_hit_particle_above(struct cell* c, double a[3], double b[3
 	return 0;
 }
 
-double  tree_does_ray_hit_particle(struct cell* c, double a[3], double b[3]){
+void  tree_does_ray_hit_particle(struct cell* c, double a[3], double b[3], double* flux, double* height){
 	double t1 = b[1]*(a[2]-c->z)-b[2]*(a[1]-c->y);
 	double t2 = b[2]*(a[0]-c->x)-b[0]*(a[2]-c->z);
 	double t3 = b[0]*(a[1]-c->y)-b[1]*(a[0]-c->x);
@@ -310,8 +310,7 @@ double  tree_does_ray_hit_particle(struct cell* c, double a[3], double b[3]){
 			for(int i=0;i<8;i++){
 				struct cell* o = c->oct[i];
 				if(o){
-					double F = tree_does_ray_hit_particle(o,a,b);
-					if (F>0) return F;
+					tree_does_ray_hit_particle(o,a,b, flux, height);
 				}
 			}
 		}else{
@@ -326,38 +325,45 @@ double  tree_does_ray_hit_particle(struct cell* c, double a[3], double b[3]){
 				double cy = a[1] + d*b[1];
 				double cz = a[2] + d*b[2];
 
-
-				double nx = cx - p.x; 		// normal to sphere
-				double ny = cy - p.y;
-				double nz = cz - p.z;
-				double n  = sqrt(nx*nx + ny*ny + nz*nz);
-				nx /= n;
-				ny /= n;
-				nz /= n;
-			
-				double cosNB = nx*sun_b[0] + ny*sun_b[1] + nz*sun_b[2];
-				if (cosNB>0){ // dayside of particle
-					// Check for shadow.
-					int nghostray = 1; // check only one ghost box for now. TODO
-					for (int gbx=-nghostray; gbx<=nghostray; gbx++){
-						for (int gby=-nghostray; gby<=nghostray; gby++){
-							struct ghostbox gb = boundaries_get_ghostbox(gbx,gby,0);
-							double inters[3]; // ray position in xy plane
-							inters[0] = cx+gb.shiftx;
-							inters[1] = cy+gb.shifty;
-							inters[2] = cz+1e-6*p.r; // avoid floating point issues
-							for (int i=0;i<root_n;i++){
-								struct cell* ce = tree_root[i];
-								if (tree_does_ray_hit_particle_above(ce,inters,sun_b)){
-									printf("in shadow\n");
-									return 0; // sphere in the shadow
-									
+				if (cz>(*height)){			// intersection happens before previous intersection
+					double nx = cx - p.x; 		// normal to sphere
+					double ny = cy - p.y;
+					double nz = cz - p.z;
+					double n  = sqrt(nx*nx + ny*ny + nz*nz);
+					nx /= n;
+					ny /= n;
+					nz /= n;
+				
+					double cosNB = nx*sun_b[0] + ny*sun_b[1] + nz*sun_b[2];
+					if (cosNB>0){ // dayside of particle
+						// Check for shadow.
+						int nghostray = 1; // check only one ghost box for now. TODO
+						int isinshadow = 0;
+						for (int gbx=-nghostray; gbx<=nghostray; gbx++){
+							for (int gby=-nghostray; gby<=nghostray; gby++){
+								struct ghostbox gb = boundaries_get_ghostbox(gbx,gby,0);
+								double inters[3]; // ray position in xy plane
+								inters[0] = cx+gb.shiftx;
+								inters[1] = cy+gb.shifty;
+								inters[2] = cz+; 
+								for (int i=0;i<root_n;i++){
+									struct cell* ce = tree_root[i];
+									if (tree_does_ray_hit_particle_above(ce,inters,sun_b,c->pt)){
+										isinshadow = 1;
+									}
 								}
 							}
 						}
+						if (isinshadow){
+							(*flux) 	= 0;
+						}else{
+							(*flux) 	= cosNB;
+						}
+						(*height)	= cz;
+					}else{
+						(*flux) 	= 0;
+						(*height)	= cz;
 					}
-									printf("not in in shadow\n");
-					return cosNB; // Lambert Sphere
 				}
 			}
 		}
@@ -387,59 +393,49 @@ void tree_get_transparency(double B, double phi){
 		nghostray++;
 	}
 
-	const int num_rays 		= 1000;
+	const int N_rays 		= 1000;
 
-	double* _a = malloc(sizeof(double)*num_rays*3);
-	int*   _t = calloc(num_rays,sizeof(int));
-	double*   _F = calloc(num_rays,sizeof(double));
-	for(int j=0;j<num_rays;j++){
-		_a[3*j+0] = tools_uniform(-boxsize_x/2.,boxsize_x/2.);
-		_a[3*j+1] = tools_uniform(-boxsize_y/2.,boxsize_y/2.); // Only really works in square boxes.
-		_a[3*j+2] = 0;
-	}
+	double*   _F = calloc(N_rays,sizeof(double));
 
+
+	int N_ray_hit_particle 	= 0;	// Number of rays that hit a particle
+	double F_reflected 	= 0.;	// Integrated flux. 
 #pragma omp parallel for 
-	for(int j=0;j<num_rays;j++){
+	for(int j=0;j<N_rays;j++){
+		double a[3]; // ray position in xy plane
+		a[0] = tools_uniform(-boxsize_x/2.,boxsize_x/2.);
+		a[1] = tools_uniform(-boxsize_y/2.,boxsize_y/2.); // Only really works in square boxes.
+		a[2] = 0;
 		int transparency = 0;
-		double flux = 0;
+		double flux = -1;	// -1 means ray passes through ring, 0 means it hits a dark particle, 1 means it hits sees the most iluminated part of a a particle
+		double height = -boxsize_z;
 		for (int gbx=-nghostray; gbx<=nghostray; gbx++){
 			for (int gby=-nghostray; gby<=nghostray; gby++){
 				struct ghostbox gb = boundaries_get_ghostbox(gbx,gby,0);
-				double a[3]; // ray position in xy plane
-				a[0] = _a[3*j+0]+gb.shiftx;
-				a[1] = _a[3*j+1]+gb.shifty;
-				a[2] = _a[3*j+2];
+				double as[3]; // ishifted ray position in xy plane
+				as[0] = a[0]+gb.shiftx;
+				as[1] = a[1]+gb.shifty;
+				as[2] = a[2];
 				for (int i=0;i<root_n;i++){
 					struct cell* c = tree_root[i];
-					flux = tree_does_ray_hit_particle(c,a,b);	
-					if (flux>0.){
-						goto raydone;
-					}
+					tree_does_ray_hit_particle(c,as,b, &flux, &height);	
 				}
 			}
 		}
-		raydone:;
-		if (flux>0){
-			_t[j] = 1;
-			_F[j] = flux;
+		if (flux>=0.){
+			N_ray_hit_particle 	+= 1;
+			F_reflected 		+= flux;
 		}
 	}
 
-	int num_intersecting = 0;
-	double F_sum = 0;
-	for(int j=0;j<num_rays;j++){
-		if (_t[j]>0) num_intersecting++;
-		F_sum += _F[j];
-	}
+
 	FILE* of = fopen("transparency.txt","a+"); 
 	if (of==NULL){
 		printf("\n\nError while opening file.\n");
 		return;
 	}
-	fprintf(of,"%e\t%e\t%e\t%0.8f\n",t,B,phi,(double)num_intersecting/(double)num_rays,(double)F_sum/(double)num_rays);
+	fprintf(of,"%e\t%e\t%e\t%0.8f\n",t,B,phi,(double)N_ray_hit_particle/(double)N_rays,(double)F_reflected/(double)N_rays);
 	fclose(of);
-	free(_a);
-	free(_t);
 }
 
 void output_transparency(){
