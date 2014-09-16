306175fc6ba254b2651a158eca75c9bd176be23d
Hanno Rein <hanno@hanno-rein.de>
2014-09-15 20:06:50 -0400

    Cleanup libias15

--- shared/libias15.c
+++ shared/libias15.c
@@ -1,3 +1,30 @@
+/**
+ * @file 	libias15.c
+ * @brief 	Declarations and functions for shared library access to the IAS15 integrator.
+ * @author 	Hanno Rein <hanno@hanno-rein.de>
+ * 		Dave Spiegel <dave@ias.edu>
+ * 
+ * @section 	LICENSE
+ * Copyright (c) 2011 Hanno Rein, Dave Spiegel
+ *
+ * This file is part of rebound.
+ *
+ * rebound is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rebound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,17 +36,22 @@
 #include "gravity.h"
 #include "particle.h"
 #include "main.h"
+#include "boundaries.h"
 
-double dt 	= 0.01;	// Default values
+// Default values of parameters and constants
+double dt 	= 0.01;	
 double t 	= 0;
 double tmax	= 0;
 double G 	= 1;
 double softening = 0;
 
+// Function pointer to additional forces
 void (*problem_additional_forces) () = NULL;
 
+// Particle structure
 struct particle* particles = NULL;
 
+// Particle getter/setter methods.
 void setp(struct particle* _p){
 	free(particles);
 	particles = malloc(sizeof(struct particle)*N);
@@ -32,7 +64,8 @@ struct particle* particles_get(){
 	return particles;
 }
 
-void step(){
+// Integrate for 1 step
+void step(){ 
 	if (N<=0){
 		fprintf(stderr,"\n\033[1mError!\033[0m No particles found. Exiting.\n");
 		return;
@@ -42,6 +75,7 @@ void step(){
 	integrator_part2();
 }
 
+// Integrate until t=_tmax
 void integrate(double _tmax){
 	tmax = _tmax;
 	double dt_last_done = dt;
@@ -60,7 +94,8 @@ void integrate(double _tmax){
 	dt = dt_last_done;
 }
 	 
-
+// The following code is needed to leave the original REBOUND files unchanged. 
+// Currently the libias15 library only supports an infinitely large box.
 // Infinite box size
 int root_nx = 1;
 int root_ny = 1;
@@ -78,14 +113,6 @@ int boundaries_particle_is_in_box(struct particle p){
 int nghostx = 0;	
 int nghosty = 0;
 int nghostz = 0;
-struct ghostbox{
-	double shiftx;		/**< Relative x position */
-	double shifty;		/**< Relative y position */
-	double shiftz;		/**< Relative z position */
-	double shiftvx;		/**< Relative x velocity */
-	double shiftvy;		/**< Relative y velocity */
-	double shiftvz;		/**< Relative z velocity */
-};
 
 struct ghostbox boundaries_get_ghostbox(int i, int j, int k){
 	struct ghostbox gb;
