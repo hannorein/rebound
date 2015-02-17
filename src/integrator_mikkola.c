bb087374313568eb4e4f199ca84809b3748cb396
Hanno Rein <hanno@hanno-rein.de>
2015-02-17 15:30:22 -0500

    implemented c functions

---
+++ src/integrator_mikkola.c
@@ -0,0 +1,128 @@
+/**
+ * @file 	integrator.c
+ * @brief 	Leap-frog integration scheme.
+ * @author 	Hanno Rein <hanno@hanno-rein.de>
+ * @detail	This file implements the leap-frog integration scheme.  
+ * This scheme is second order accurate, symplectic and well suited for 
+ * non-rotating coordinate systems. Note that the scheme is formally only
+ * first order accurate when velocity dependent forces are present.
+ * 
+ * @section 	LICENSE
+ * Copyright (c) 2011 Hanno Rein, Shangfei Liu
+ *
+ * This file is part of rebound.
+ *
+ * rebound is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rebound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+#include "particle.h"
+#include "main.h"
+#include "gravity.h"
+#include "boundaries.h"
+
+// These variables have no effect for leapfrog.
+int integrator_force_is_velocitydependent 	= 1;
+double integrator_epsilon 			= 0;
+double integrator_min_dt 			= 0;
+
+// Fast factorial lookup table
+static const double factorial[] = {1., 1., 2., 6., 24., 120., 720., 5040., 40320., 362880., 3628800., 39916800., 479001600., 6227020800., 87178291200., 1307674368000., 20922789888000., 355687428096000., 6402373705728000., 121645100408832000., 2432902008176640000., 51090942171709440000., 1124000727777607680000., 25852016738884976640000., 620448401733239439360000., 15511210043330985984000000., 403291461126605635584000000., 10888869450418352160768000000., 304888344611713860501504000000., 8841761993739701954543616000000., 265252859812191058636308480000000., 8222838654177922817725562880000000., 263130836933693530167218012160000000., 8683317618811886495518194401280000000., 295232799039604140847618609643520000000.};
+
+double c_n_series(unsigned int n, double z){
+	double c_n = 0.;
+	for (unsigned int j=0;j<13;j++){
+		double term = pow(-z,j)/factorial[n+2*j];
+		c_n += term;
+		if (fabs(term/c_n) < 1e-17) break; // Stop if new term smaller than machine precision
+	}
+	return c_n;
+}
+
+double c(unsigned int n, double z){
+	if (z>0.5){
+		switch(n){
+			case 0:
+			{
+				double cn4 = c(3,z/4.)*(1.+c(1,z/4.))/8.;
+				double cn2 = 1.-z*cn4;
+				double cn0 = 1.-z*cn2;
+				return cn0;
+			}
+			case 1:
+			{
+				double cn5 = (c(5,z/4.)+c(4,z/4.)+c(3,z/4.)*c(2,z/4))/16.;
+				double cn3 = 1.-z*cn5;
+				double cn1 = 1.-z*cn3;
+				return cn1;
+			}
+			case 2:
+			{
+				double cn4 = c(3,z/4.)*(1.+c(1,z/4.))/8.;
+				double cn2 = 1.-z*cn4;
+				return cn2;
+			}
+			case 3:
+			{
+				double cn5 = (c(5,z/4.)+c(4,z/4.)+c(3,z/4.)*c(2,z/4))/16.;
+				double cn3 = 1.-z*cn5;
+				return cn3;
+			}
+			case 4:
+			{
+				double cn4 = c(3,z/4.)*(1.+c(1,z/4.))/8.;
+				return cn4;
+			}
+			case 5:
+			{
+				double cn5 = (c(5,z/4.)+c(4,z/4.)+c(3,z/4.)*c(2,z/4))/16.;
+				return cn5;
+			}
+		}
+	}
+	return c_n_series(n,z);
+}
+
+
+// Leapfrog integrator (Drift-Kick-Drift)
+// for non-rotating frame.
+void integrator_part1(){
+	printf("\n C(t)=%f",c_n_series(2,t));
+#pragma omp parallel for schedule(guided)
+	for (int i=0;i<N;i++){
+		particles[i].x  += 0.5* dt * particles[i].vx;
+		particles[i].y  += 0.5* dt * particles[i].vy;
+		particles[i].z  += 0.5* dt * particles[i].vz;
+	}
+	t+=dt/2.;
+}
+void integrator_part2(){
+#pragma omp parallel for schedule(guided)
+	for (int i=0;i<N;i++){
+		particles[i].vx += dt * particles[i].ax;
+		particles[i].vy += dt * particles[i].ay;
+		particles[i].vz += dt * particles[i].az;
+		particles[i].x  += 0.5* dt * particles[i].vx;
+		particles[i].y  += 0.5* dt * particles[i].vy;
+		particles[i].z  += 0.5* dt * particles[i].vz;
+	}
+	t+=dt/2.;
+}
+	
+
