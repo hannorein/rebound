644b693314eccc4316b4802e5ff5275fcf8c2a6c
Hanno Rein <hanno@hanno-rein.de>
2015-03-16 13:53:25 -0400

    inflight tests

--- python_examples/2bodyHyperbolic/hyperbolictest.py
+++ python_examples/2bodyHyperbolic/hyperbolictest.py
@@ -6,18 +6,18 @@ import numpy as np
 from interruptible_pool import InterruptiblePool
 
 
-for i in np.linspace(-1.67,1.67,100):
+for i in np.linspace(-2.*np.pi,2.*np.pi,1000):
     rebound.reset()
     rebound.set_integrator("mikkola")
-    rebound.set_dt(0.0002*2.*np.pi)
+    rebound.set_dt(0.01*2.*np.pi)
 
     try:
         rebound.particle_add(m=1.)
-        rebound.particle_add(m=0., a=1., e=11, anom=i)
+        rebound.particle_add(m=0., a=1., e=1.01, anom=i)
         particles = rebound.particles_get()
-        print particles[1].x, particles[1].y
-        rebound.step()
-        print particles[1].x, particles[1].y
+        print particles[1].x, particles[1].y, i
+        #rebound.step()
+        #print particles[1].x, particles[1].y, i
     except:
         pass
     
