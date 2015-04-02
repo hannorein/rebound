Fixed MEGNO calculation. Now we have x/v/a all present AND at same time.

python_examples/megno_simple/problem.py
@@ -17,7 +17,7 @@ def simulation(integrator):
     f = open(integrator+".txt","w")
     rebound.reset()
     rebound.set_integrator(integrator)
-    rebound.set_dt(0.02)
+    rebound.set_dt(0.01)
         
     rebound.add_particle(m=1.)
     rebound.add_particle(m=0.01, a=1,e=0.1)
@@ -26,9 +26,10 @@ def simulation(integrator):
     rebound.move_to_center_of_momentum()
     rebound.init_megno(1e-10)
     particles = rebound.get_particles()
-    for x in xrange(100000):
-        rebound.step()
-        print("%e %e %e %e %e\n" %(rebound.get_t(), rebound.get_megno(), particles[3].x, particles[4].x, particles[5].x),file=f)
+    times = np.logspace(2,5,num=1000)
+    for t in times:
+        rebound.integrate(t,0)
+        print("%e %e %e %e %e %e %e %e\n" %(rebound.get_t(), rebound.get_megno(), particles[0].x, particles[1].x, particles[2].x, particles[3].x, particles[4].x, particles[5].x),file=f)
 
-simulation("ias15")
+#simulation("ias15")
 simulation("mikkola")