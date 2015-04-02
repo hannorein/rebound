5e8ea8893ebcd12ff82d79195d344572ffee2a92
Hanno Rein <hanno@hanno-rein.de>
2015-04-02 13:47:04 -0400

    Optimized double sum for variational equations. Hard to to for normal particles because of compensated summation

--- src/gravity_direct.c
+++ src/gravity_direct.c
@@ -55,19 +55,19 @@ void gravity_calculate_acceleration(){
 	}
 	// Summing over all Ghost Boxes
 	const int _N_active = ((N_active==-1)?N:N_active)- N_megno;
+	const int _N_start  = (selected_integrator==2?1:0);
 	for (int gbx=-nghostx; gbx<=nghostx; gbx++){
 	for (int gby=-nghosty; gby<=nghosty; gby++){
 	for (int gbz=-nghostz; gbz<=nghostz; gbz++){
 		struct ghostbox gb = boundaries_get_ghostbox(gbx,gby,gbz);
 		// Summing over all particle pairs
 #pragma omp parallel for schedule(guided)
-		for (int i=(selected_integrator==2?1:0); i<_N_real; i++){
+		for (int i=_N_start; i<_N_real; i++){
 			double csx = 0;
 			double csy = 0;
 			double csz = 0;
-		for (int j=(selected_integrator==2?1:0); j<_N_active; j++){
-			if (selected_integrator==1 && i==1 && j==0) continue;
-			if (selected_integrator==1 && j==1 && i==0) continue;
+		for (int j=_N_start; j<_N_active; j++){
+			if (selected_integrator==1 && ((i==1 && j==0) || (j==1 && i==0)) ) continue;
 			if (i==j) continue;
 			double dx = (gb.shiftx+particles[i].x) - particles[j].x;
 			double dy = (gb.shifty+particles[i].y) - particles[j].y;
@@ -115,10 +115,8 @@ void gravity_calculate_variational_acceleration(){
 	}
 #pragma omp parallel for schedule(guided)
 	for (int i=_N_real; i<N; i++){
-	for (int j=_N_real; j<N; j++){
-		if (selected_integrator==1 && i==_N_real+1 && j==_N_real) continue;
-		if (selected_integrator==1 && j==_N_real+1 && i==_N_real) continue;
-		if (i==j) continue;
+	for (int j=i+1; j<N; j++){
+		if (selected_integrator==1 && ((i==_N_real+1 && j==_N_real) || (j==_N_real+1 && i==_N_real)) ) continue;
 		const double dx = particles[i-N/2].x - particles[j-N/2].x;
 		const double dy = particles[i-N/2].y - particles[j-N/2].y;
 		const double dz = particles[i-N/2].z - particles[j-N/2].z;
@@ -128,26 +126,27 @@ void gravity_calculate_variational_acceleration(){
 		const double ddx = particles[i].x - particles[j].x;
 		const double ddy = particles[i].y - particles[j].y;
 		const double ddz = particles[i].z - particles[j].z;
-		const double Gm = G * particles[j].m;
+		const double Gmi = G * particles[i].m;
+		const double Gmj = G * particles[j].m;
 		
 		// Variational equations
-		particles[i].ax += Gm * (
-			+ ddx * ( dx*dx*r5inv - r3inv )
-			+ ddy * ( dx*dy*r5inv )
-			+ ddz * ( dx*dz*r5inv )
-			);
-
-		particles[i].ay += Gm * (
-			+ ddx * ( dy*dx*r5inv )
-			+ ddy * ( dy*dy*r5inv - r3inv )
-			+ ddz * ( dy*dz*r5inv )
-			);
-
-		particles[i].az += Gm * (
-			+ ddx * ( dz*dx*r5inv )
-			+ ddy * ( dz*dy*r5inv )
-			+ ddz * ( dz*dz*r5inv - r3inv )
-			);
+		const double dax =   ddx * ( dx*dx*r5inv - r3inv )
+				   + ddy * ( dx*dy*r5inv )
+				   + ddz * ( dx*dz*r5inv );
+		const double day =   ddx * ( dy*dx*r5inv )
+				   + ddy * ( dy*dy*r5inv - r3inv )
+				   + ddz * ( dy*dz*r5inv );
+		const double daz =   ddx * ( dz*dx*r5inv )
+				   + ddy * ( dz*dy*r5inv )
+				   + ddz * ( dz*dz*r5inv - r3inv );
+		
+		particles[i].ax += Gmj * dax;
+		particles[i].ay += Gmj * day;
+		particles[i].az += Gmj * daz;
+		
+		particles[j].ax -= Gmi * dax;
+		particles[j].ay -= Gmi * day;
+		particles[j].az -= Gmi * daz;
 	}
 	}
 }
