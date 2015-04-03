03a540897237119e1de94d6749e199b9535c4110
Hanno Rein <hanno@hanno-rein.de>
2015-04-03 16:24:13 -0400

    makinginteractive demo

---
+++ python_examples/megno_interactive/problem.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# This example integrates Jupiter and Saturn in the Solar system for a variety of initial conditions.
+# Alongside the normal equations of motions, IAS15 is used to integrate the variational equations.
+# These can be used to measure the Mean Exponential Growth of Nearby Orbits (MEGNO), a chaos indicator.
+# This example script runs 12^2 simulations and plots the MEGNO value. Values close to <Y>=2 correspond 
+# to regular quasi-periodic orbits. Higher values of <Y> correspond to chaotic orbits.
+ 
+# Import the rebound module
+import sys; sys.path.append('../../python_modules')
+import rebound
+from interruptible_pool import InterruptiblePool
+# Import other modules
+import numpy as np
+import multiprocessing
+
+# Runs one simulation.
+def simulation(par):
+    saturn_a, saturn_e = par
+    rebound.reset()
+    rebound.set_integrator("mikkola")
+    rebound.set_masses_are_constant(1)
+    rebound.set_min_dt(5.)
+    rebound.set_dt(1.)
+    
+    # These parameters are only approximately those of Jupiter and Saturn.
+    sun     = rebound.Particle(m=1.)
+    rebound.add_particle(sun)
+    jupiter = rebound.add_particle(primary=sun,m=0.000954, a=5.204, anom=0.600, omega=0.257, e=0.048)
+    saturn  = rebound.add_particle(primary=sun,m=0.000285, a=saturn_a, anom=0.871, omega=1.616, e=saturn_e)
+
+    rebound.move_to_center_of_momentum()
+    rebound.init_megno(1e-16)
+    rebound.integrate(1e3*2.*np.pi)
+
+    return [rebound.get_megno(),1./(rebound.get_lyapunov()*2.*np.pi)] # returns MEGNO and Lypunov timescale in years
+
+
+### Setup grid and run many simulations in parallel
+N = 2                   # Grid size, increase this number to see more detail
+a = np.linspace(7.,10.,N)   # range of saturn semi-major axis in AU
+e = np.linspace(0.,0.5,N)   # range of saturn eccentricity
+parameters = [[_a, _e] for _a in a for _e in e]
+
+print a
+b = np.linspace((a[0]+a[1])/2.,a[-1],len(a)-1, endpoint=False)
+a = np.sort(np.concatenate((a,b)))
+print a
+b = np.linspace((a[0]+a[1])/2.,a[-1],len(a)-1, endpoint=False)
+a = np.sort(np.concatenate((a,b)))
+print a
+exit(0)
+
+
+# Run simulations in parallel
+pool = InterruptiblePool()    # Number of threads default to the number of CPUs on the system
+print("Running %d simulations on %d threads..." % (len(parameters), pool._processes))
+res = np.nan_to_num(np.array(pool.map(simulation,parameters))) 
+megno = np.clip(res[:,0].reshape((N,N)),1.8,4.)             # clip arrays to plot saturated 
+lyaptimescale = np.clip(np.absolute(res[:,1].reshape((N,N))),1e1,1e5)
+
+### Create plot and save as pdf 
+import matplotlib.pyplot as plt
+plt.ion()
+from matplotlib.colors import LogNorm
+
+# Setup plots
+f, axarr = plt.subplots(2,figsize=(10,10))
+extent = [a.min(), a.max(), e.min(), e.max()]
+for ax in axarr:
+    ax.set_xlim(extent[0],extent[1])
+    ax.set_ylim(extent[2],extent[3])
+    ax.set_xlabel("$a_{\mathrm{Saturn}}$ [AU]")
+    ax.set_ylabel("$e_{\mathrm{Saturn}}$")
+
+
+# Plot MEGNO 
+im1 = axarr[0].imshow(megno, vmin=1.8, vmax=4., aspect='auto', origin="lower", interpolation='nearest', cmap="RdYlGn_r", extent=extent)
+cb1 = plt.colorbar(im1, ax=axarr[0])
+cb1.solids.set_rasterized(True)
+cb1.set_label("MEGNO $\\langle Y \\rangle$")
+
+# Plot Lyapunov timescale
+im2 = axarr[1].imshow(lyaptimescale, vmin=1e1, vmax=1e5, norm=LogNorm(), aspect='auto', origin="lower", interpolation='nearest', cmap="RdYlGn", extent=extent)
+cb2 = plt.colorbar(im2, ax=axarr[1])
+cb2.solids.set_rasterized(True)
+cb2.set_label("Lyapunov timescale [years]")
+plt.draw()
+
+print "hello"
+raw_input('Press enter...')
