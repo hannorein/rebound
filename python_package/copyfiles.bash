e8ec42b1840281217003d1fa3d010269075c4cab
Hanno Rein <hanno@hanno-rein.de>
2015-04-11 15:14:17 -0400

    Adding copy files script.

---
+++ python_package/copyfiles.bash
@@ -0,0 +1,6 @@
+#/bin/bash
+cp ../README.md .
+cp ../shared/librebound/librebound.c librebound/
+cp ../shared/librebound/librebound.h librebound/
+cp ../src/*.c librebound/
+cp ../src/*.h librebound/
