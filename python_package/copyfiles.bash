a8e82c3f9ca5bdd9c2f328a1a2e8389d9b632c18
Hanno Rein <hanno@hanno-rein.de>
2015-04-11 23:35:01 -0400

    Moved python module to main directory. This seems the only reasonable way to handle this as distutils doesn't understand symbolic links

--- python_package/copyfiles.bash
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-cp ../README.rst .
-cp ../shared/librebound/librebound.c librebound/
-cp ../shared/librebound/librebound.h librebound/
-cp ../src/*.c librebound/
-cp ../src/*.h librebound/
