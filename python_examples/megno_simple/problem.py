be7b1bcd0f4fa7bd2a3bd99b3ba9ec1ead3f7c9a
Hanno Rein <hanno@hanno-rein.de>
2015-04-19 21:26:26 -0400

    Updated megno_simple example

--- python_examples/megno_simple/problem.py
+++ python_examples/megno_simple/problem.py
@@ -5,31 +5,33 @@
 # This example script runs 12^2 simulations and plots the MEGNO value. Values close to <Y>=2 correspond 
 # to regular quasi-periodic orbits. Higher values of <Y> correspond to chaotic orbits.
 from __future__ import print_function
- 
 # Import the rebound module
-import sys; sys.path.append('../../python_modules')
 import rebound
-from interruptible_pool import InterruptiblePool
+from rebound.interruptible_pool import InterruptiblePool
 # Import other modules
 import numpy as np
 
 def simulation(integrator):
-    f = open(integrator+".txt","w")
-    rebound.reset()
-    rebound.set_integrator(integrator)
-    rebound.set_dt(0.2)
-        
-    rebound.add_particle(m=1.)
-    rebound.add_particle(m=0.01, a=1,e=0.1)
-    rebound.add_particle(m=0.01, a=2.)
+    print("Running "+integrator)
+    with open(integrator+".txt","w") as f:
+        rebound.reset()
+        rebound.set_integrator(integrator)
+        rebound.set_dt(0.2)
+            
+        rebound.add_particle(m=1.)
+        rebound.add_particle(m=0.01, a=1,e=0.1)
+        rebound.add_particle(m=0.01, a=2.)
 
-    rebound.move_to_center_of_momentum()
-    rebound.init_megno(1e-10)
-    particles = rebound.get_particles()
-    times = np.logspace(2,5,num=1000)
-    for t in times:
-        rebound.integrate(t,0)
-        print("%e %e %e %e %e %e %e %e\n" %(rebound.get_t(), rebound.get_megno(), particles[0].x, particles[1].x, particles[2].x, particles[3].x, particles[4].x, particles[5].x),file=f)
+        rebound.move_to_center_of_momentum()
+        rebound.init_megno(1e-10)
+        particles = rebound.get_particles()
+        times = np.logspace(2,5,num=1000)
+        for t in times:
+            rebound.integrate(t,0)
+            print("%e %e %e %e %e %e %e %e\n" %(rebound.get_t(), rebound.get_megno(), particles[0].x, particles[1].x, particles[2].x, particles[3].x, particles[4].x, particles[5].x),file=f)
 
-#simulation("ias15")
+simulation("ias15")
 simulation("mikkola")
+simulation("mikkola-cor3")
+simulation("mikkola-cor5")
+simulation("mikkola-cor11")
--- rebound/__init__.py
+++ rebound/__init__.py
@@ -504,8 +504,17 @@ def set_integrator(integrator="IAS15"):
         if integrator.lower() == "ias15":
             set_integrator(0)
             return
-        if integrator.lower() == "mikkola":
+        if integrator[0:7].lower() == "mikkola":
             set_integrator(1)
+            set_integrator_mikkola_corrector(0)
+            if integrator[9:] == "cor3":
+                set_integrator_mikkola_corrector(3)
+            if integrator[9:] == "cor5":
+                set_integrator_mikkola_corrector(5)
+            if integrator[9:] == "cor7":
+                set_integrator_mikkola_corrector(7)
+            if integrator[9:] == "cor11":
+                set_integrator_mikkola_corrector(11)
             return
         if integrator.lower() == "sei":
             set_integrator(2)
