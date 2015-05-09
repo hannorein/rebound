cbfb18d53ee034cd98335bda2ca0145ca513c212
Hanno Rein <hanno@hanno-rein.de>
2015-05-09 08:58:21 -0400

    test case working

--- python_examples/horizons/problem.py
+++ python_examples/horizons/problem.py
@@ -1,5 +1,11 @@
 import rebound
-rebound.add("SSB")
-rebound.add("EMB") # Earth Moon Barycenter
-rebound.add("Mars Barycenter") 
+# Need to specify mass as HORIZONS doesn't return physical properties in easily readable format.
+# Optionally set date with argument date="2015-05-08 12:00" 
+rebound.add("Sun",m=1.)
+rebound.add("EMB") # Earth Moon Barycenter (sometimes stupid name convention)
+rebound.add("Jupiter Barycenter",m=0.0009547919) 
+rebound.add("C/2014 Q2") # Comets!
+rebound.status()
+rebound.set_integrator("whfast")
+rebound.integrate(100.)
 rebound.status()
--- rebound/__init__.py
+++ rebound/__init__.py
@@ -31,7 +31,7 @@ except ImportError:
 
 TINY=1.e-308
 MIN_REL_ERROR = 1.e-12
-INITTIME = datetime.datetime.today()
+INITDATE = datetime.datetime.today()
 
 
 def get_build_str():
@@ -414,7 +414,7 @@ def set_particles(_particles):
     arr = (Particle * len(_particles))(*_particles)
     librebound.setp(byref(arr))
 
-def add_particle(particle=None, m=None, x=None, y=None, z=None, vx=None, vy=None, vz=None, primary=None, a=None, anom=None, e=None, omega=None, inc=None, Omega=None, MEAN=None):   
+def add_particle(particle=None, m=None, x=None, y=None, z=None, vx=None, vy=None, vz=None, primary=None, a=None, anom=None, e=None, omega=None, inc=None, Omega=None, MEAN=None, date=None):   
     """Adds a particle to REBOUND. Accepts one of the following four sets of arguments:
     1) A single Particle structure.
     2) A list of Particle structures.
@@ -427,26 +427,30 @@ def add_particle(particle=None, m=None, x=None, y=None, z=None, vx=None, vy=None
         for particle in particle:
             librebound.particles_add(particle)
     elif isinstance(particle,str):
-        print("Trying to get data from NASA Horizons...")
-        
+        if date is not None:
+            date = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M")
+        else:
+            date = INITDATE
+        print("Trying to get data from NASA Horizons for body '%s' at date '%s'..."%(particle,date.strftime("%Y-%m-%d %H:%M")))
 
         t = telnetlib.Telnet()
         t.open('horizons.jpl.nasa.gov', 6775)
-
         expect = ( ( r'Horizons>', particle+'\n' ),
                    ( r'Continue.*:', 'y\n' ),
                    ( r'Select.*E.phemeris.*:', 'E\n'),
                    ( r'Observe.*:', 'v\n' ),
                    ( r'Coordinate center.*:', '@Sun\n' ),
                    ( r'Reference plane.*:', 'eclip\n' ),
-                   ( r'Starting.* :', INITTIME.strftime("%Y-%m-%d %H:%M")+'\n' ),
-                   ( r'Ending.* :', (INITTIME + datetime.timedelta(hours=1)).strftime("%Y-%m-%d %H:%M")+'\n' ),
+                   ( r'Starting.* :', date.strftime("%Y-%m-%d %H:%M")+'\n' ),
+                   ( r'Ending.* :', (date + datetime.timedelta(hours=1)).strftime("%Y-%m-%d %H:%M")+'\n' ),
                    ( r'Output interval.*:', '1d\n' ),
                    ( r'Accept default output.*:', 'y\n' ),
                    ( r'Scroll . Page: .*%', ' '),
                    ( r'Select\.\.\. .A.gain.* :', 'X\n' )
         )
         p = Particle()
+        startdata = 0
+        message = ""
         while True:
             try:
                 answer = t.expect(list(i[0] for i in expect), 4)
@@ -454,7 +458,6 @@ def add_particle(particle=None, m=None, x=None, y=None, z=None, vx=None, vy=None
                 break
             if "$$SOE" in answer[2]:
                 lines = answer[2].split("\n")
-                startdata = 0
                 for line in lines:
                     if line.strip() == "$$EOE":
                         break
@@ -465,14 +468,25 @@ def add_particle(particle=None, m=None, x=None, y=None, z=None, vx=None, vy=None
                         p.z = pos[2]
                     if startdata == 3:
                         vel = [float(i) for i in line.split()]
-                        p.vx = vel[0]
-                        p.vy = vel[1]
-                        p.vz = vel[2]
+                        auperday2auperyeartwopi = 58.130101
+                        p.vx = vel[0]*auperday2auperyeartwopi 
+                        p.vy = vel[1]*auperday2auperyeartwopi 
+                        p.vz = vel[2]*auperday2auperyeartwopi 
                     if startdata > 0:
                         startdata += 1
+                    if line[0:18] == "Target body name: ":
+                        print("Found: %s" % line[18:50].strip())
                     if line.strip() == "$$SOE":
                         startdata = 1
+            message += answer[2].replace(chr(27)+"[H","")
             t.write(expect[answer[0]][1])
+        if startdata == 0:
+            print(message)
+            raise SyntaxError("Object not found. See above output from HORIZONS. Please try different identifier or look up JPL Body Number.")
+        if m is not None:
+            p.m = m
+        else:
+            print("Mass cannot be retrieved from NASA HORIZONS. Please add manually.")
         add_particle(p)
     else:
        librebound.particles_add(particle)
