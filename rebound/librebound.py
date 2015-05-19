e6e28ba6a33701b41c490f44cddc9b09842a33c9
Hanno Rein <hanno@hanno-rein.de>
2015-05-19 11:05:08 -0400

    Added calculate_energy()

--- rebound/librebound.py
+++ rebound/librebound.py
@@ -288,6 +288,10 @@ class ReboundModule(types.ModuleType):
 # Tools
     def move_to_com(self):
         self.clibrebound.tools_move_to_center_of_momentum()
+    
+    def calculate_energy(self):
+        self.clibrebound.tools_energy.restype = c_double
+        return self.clibrebound.tools_energy()
 
     def reset(self):
         debug.reset_debug()
