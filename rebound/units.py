b1d899e21387cda47feb0da80855d731257123fd
dtamayo <tamayo.daniel@gmail.com>
2015-05-27 00:26:58 -0400

    finished fourier tutorial, updated __init__ to include Orbit class

--- rebound/units.py
+++ rebound/units.py
@@ -3,7 +3,11 @@
 Functions for converting units
 """
 
-units = {'m':1.,
+import rebound
+
+units = [None, None, None]
+
+unit_dic = {'m':1.,
         'km':1000.,
         'AU':1.5e11,
         \
@@ -16,13 +20,23 @@ units = {'m':1.,
         }
 
 def convert(val, oldunits, newunits):
-    return val*units[oldunits]/units[newunits]
+    return val*unit_dic[oldunits]/unit_dic[newunits]
 
 def convert_vel(val, oldlength, oldtime, newlength, newtime):
-    in_SI_units=val*units[oldlength]/units[oldtime]
-    return in_SI_units*units[newtime]/units[newlength]
+    in_SI_units=val*unit_dic[oldlength]/unit_dic[oldtime]
+    return in_SI_units*unit_dic[newtime]/unit_dic[newlength]
 
 def convert_G(val, oldlength, oldtime, oldmass, newlength, newtime, newmass):
-    in_SI_units=val*units[oldlength]**3/units[oldmass]/units[oldtime]**2
-    return in_SI_units*units[newmass]*units[newtime]**2/units[newlength]**3
+    in_SI_units=val*unit_dic[oldlength]**3/unit_dic[oldmass]/unit_dic[oldtime]**2
+    return in_SI_units*unit_dic[newmass]*unit_dic[newtime]**2/unit_dic[newlength]**3
 
+def set_sim_units(length, time, mass)
+def _units(length, time, mass):
+    if sim_unit_dic[0] is None:
+        if rebound.particles is not []:
+            print("Error:  Have to set unit_dic BEFORE calling set_sim_unit_dic.  See unit_dic.ipynb in python_tutorials.")
+            exit()
+
+        sim_unit_dic = length, time, mass
+        print(sim_unit_dic)
+            
