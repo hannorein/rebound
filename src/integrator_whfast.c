44a98a41137747553d6ac3603b35209674e22602
Hanno Rein <hanno@hanno-rein.de>
2015-06-19 13:39:04 -0400

    Added warning message.

--- src/integrator_whfast.c
+++ src/integrator_whfast.c
@@ -55,7 +55,7 @@ static double* restrict etai = NULL;
 static double Mtotali;
 static double Mtotal;
 static unsigned int integrator_timestep_warning = 0;
-static unsigned int integrator_synchronized_megno_warning = 0;
+static unsigned int integrator_whfast_recalculate_jacobi_but_not_synchronized_warning = 0;
 
 // Fast inverse factorial lookup table
 static const double invfactorial[35] = {1., 1., 1./2., 1./6., 1./24., 1./120., 1./720., 1./5040., 1./40320., 1./362880., 1./3628800., 1./39916800., 1./479001600., 1./6227020800., 1./87178291200., 1./1307674368000., 1./20922789888000., 1./355687428096000., 1./6402373705728000., 1./121645100408832000., 1./2432902008176640000., 1./51090942171709440000., 1./1124000727777607680000., 1./25852016738884976640000., 1./620448401733239439360000., 1./15511210043330985984000000., 1./403291461126605635584000000., 1./10888869450418352160768000000., 1./304888344611713860501504000000., 1./8841761993739701954543616000000., 1./265252859812191058636308480000000., 1./8222838654177922817725562880000000., 1./263130836933693530167218012160000000., 1./8683317618811886495518194401280000000., 1./295232799039604140847618609643520000000.};
@@ -746,7 +746,10 @@ void integrator_whfast_part1(){
 	if (integrator_whfast_safe_mode || integrator_whfast_recalculate_jacobi_this_timestep){
 		if (integrator_whfast_is_synchronized==0){
 			integrator_whfast_synchronize();
-			//fprintf(stderr,"\n\033[1mWarning!\033[0m Need to recalculate Jacobi coordinates but pos/vel were not synchronized.\n");
+			if (integrator_whfast_recalculate_jacobi_but_not_synchronized_warning==0){
+				fprintf(stderr,"\n\033[1mWarning!\033[0m Recalculating Jacobi coordinates but pos/vel were not synchronized before.\n");
+				integrator_whfast_recalculate_jacobi_but_not_synchronized_warning++;
+			}
 		}
 		eta[0] = particles[0].m;
 		etai[0] = 1./eta[0];
@@ -877,7 +880,7 @@ void integrator_whfast_reset(void){
 	integrator_whfast_recalculate_jacobi_this_timestep = 0;
 	integrator_allocated_N = 0;
 	integrator_timestep_warning = 0;
-	integrator_synchronized_megno_warning = 0;
+	integrator_whfast_recalculate_jacobi_but_not_synchronized_warning = 0;
 	free(p_j);
 	p_j = NULL;
 	free(eta);
