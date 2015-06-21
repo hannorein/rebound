f2122d0b40cae544b5ed92ce878e252dc802b973
Hanno Rein <hanno@hanno-rein.de>
2015-06-20 22:26:42 -0400

    Added ipynb2py converter

---
+++ python_tutorials/ipynb2py.py
@@ -0,0 +1,17 @@
+import json
+import sys
+
+if len(sys.argv)!=2:
+    print "Usage: ipynb2py.py FILENAME"
+    exit(1)
+with open(sys.argv[1]) as data_file:    
+    ipynb = json.load(data_file)
+
+code = ""
+for c in ipynb["cells"]:
+    if c["cell_type"] == "code":
+        source = c["source"]
+        for s in source:
+            if s[0] != "%":
+                code += s.rstrip('\n')+"\n"
+exec code
