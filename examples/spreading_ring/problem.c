057035d58ba7988836418cbee0e43810840cffd2
Hanno Rein <hanno@hanno-rein.de>
2015-07-19 10:39:17 -0400

    spreading ring

--- examples/spreading_ring/problem.c
+++ examples/spreading_ring/problem.c
@@ -1,61 +1,31 @@
 /**
- * @file 	problem.c
- * @brief 	Example problem: spreading ring.
- * @author 	Hanno Rein <hanno@hanno-rein.de>
- * @detail 	A narrow ring of collisional particles is spreading.
- * The example uses the Wisdom Holman integrator. A plane-sweep algorithm 
- * in the phi direction is used to detect collisions.
- * 
- * @section 	LICENSE
- * Copyright (c) 2011 Hanno Rein, Shangfei Liu
+ * Spreading ring
  *
- * This file is part of rebound.
- *
- * rebound is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * rebound is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ * A narrow ring of collisional particles is spreading.
  *
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
-#include <time.h>
-#include <sys/time.h>
-#include "main.h"
-#include "particle.h"
-#include "boundaries.h"
-#include "output.h"
-#include "communication_mpi.h"
-#include "tree.h"
-#include "collisions.h"
-#include "tools.h"
-#include "integrator.h"
+#include "rebound.h"
 
-extern double opening_angle2;
-extern int Nmax;
+void heartbeat(struct reb_simulation* r);
 
 int main(int argc, char* argv[]){
+	struct reb_simulation* r = reb_create_simulation();
 	// Setup constants
-	integrator	= WH;
-	G 		= 1;		
-	N_active	= 1;
-	N_collisions	= 1; 	// Don't detect collisions for the star.
-	softening 	= 0.01;		
-	dt 		= 1e-3;
-	boxsize 	= 2.4;
-	root_nx = 1; root_ny = 1; root_nz = 1;
-	nghostx = 0; nghosty = 0; nghostz = 0; 		
-	init_box();
+	r->integrator	= REB_INTEGRATOR_LEAPFROG;
+	r->collision	= REB_COLLISION_TREE;
+	r->boundary	= REB_BOUNDARY_OPEN;
+	r->G 		= 1;		
+	r->N_active	= 1;
+	r->softening 	= 0.01;		
+	r->dt 		= 1e-3;
+	r->heartbeat	= heartbeat;
+	
+	double boxsize = 4.8;
+	reb_configure_box(r, boxsize, 1, 1, 1);
 
 	// Setup particles
 	int _N = 1000;
@@ -67,14 +37,15 @@ int main(int argc, char* argv[]){
 	star.m 		= 1;
 	star.r		= 0.01;
 	reb_add(r, star);
-	while(N<_N){
+
+	while(r->N<_N){
 		struct reb_particle pt;
 		double a	= reb_random_powerlaw(boxsize/2.9,boxsize/3.1,.5);
 		double phi 	= reb_random_uniform(0,2.*M_PI);
 		pt.x 		= a*cos(phi);
 		pt.y 		= a*sin(phi);
 		pt.z 		= a*reb_random_normal(0.0001);
-		double vkep 	= sqrt(G*star.m/a);
+		double vkep 	= sqrt(r->G*star.m/a);
 		pt.vx 		=  vkep * sin(phi);
 		pt.vy 		= -vkep * cos(phi);
 		pt.vz 		= 0;
@@ -85,23 +56,12 @@ int main(int argc, char* argv[]){
 		pt.r 		= .3/sqrt((double)_N);
 		reb_add(r, pt);
 	}
+
+	reb_integrate(r, INFINITY);
 }
 
 void heartbeat(struct reb_simulation* r){
-	if (reb_output_check(10.0*dt)){
-		reb_output_timing();
+	if (reb_output_check(r, 0.0*r->dt)){
+		reb_output_timing(r, 0);
 	}
-	for (int i=N_active;i<N;i++){
-		struct reb_particle p = particles[i];
-		double r = sqrt(p.x*p.x + p.y*p.y + p.z*p.z);
-		// Remove particles falling into the star.
-		if (r<0.03){
-			particles[i] = particles[N-1];
-			i--;
-			N--;
-		}
-	}
-}
-
-void problem_finish(){
 }
