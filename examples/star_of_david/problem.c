2538cca3530298ab117f94f063bb6c21fead8518
Hanno Rein <hanno@hanno-rein.de>
2015-07-19 10:41:35 -0400

    Star of David

--- examples/star_of_david/problem.c
+++ examples/star_of_david/problem.c
@@ -1,51 +1,24 @@
 /**
- * @file 	problem.c
- * @brief 	Example problem: Star of David.
- * @author 	Hanno Rein <hanno@hanno-rein.de>
- * @detail 	This example uses the IAS15 integrator
+ * Star of David
+ * 
+ * This example uses the IAS15 integrator
  * to integrate the "Star od David", a four body system consisting of two
  * binaries orbiting each other. Note that the time is running backwards,
  * which illustrates that IAS15 can handle both forward and backward in time
- * integrations. The initial conditions are by Robert Vanderbei. For more 
- * information see http://www.princeton.edu/%7Ervdb/WebGL/New.html
- *
- * @section 	LICENSE
- * Copyright (c) 2014 Hanno Rein, Robert Vanderbei
- *
- * This file is part of rebound.
- *
- * rebound is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * rebound is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ * integrations. The initial conditions are by Robert Vanderbei.
  *
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
-#include <time.h>
-#include "main.h"
-#include "output.h"
-#include "tools.h"
-#include "particle.h"
-#include "boundaries.h"
-#include "integrator.h"
-
+#include "rebound.h"
 
 
 int main(int argc, char* argv[]){
-	integrator = IAS15;
-	init_boxwidth(6.); 			
-	dt = -1;
+	struct reb_simulation* r = reb_create_simulation();
+	r->integrator = REB_INTEGRATOR_IAS15;
+	r->dt = -1;
 
 	struct reb_particle p = {.m = 1., .z = 0., .vz = 0.};
 	
@@ -65,14 +38,7 @@ int main(int argc, char* argv[]){
 	p.vx =   0.012073765486548; p.vy =  -0.021537467220014; 
 	reb_add(r, p);
 	
-	reb_move_to_com();
-}
+	reb_move_to_com(r);
 
-void heartbeat(struct reb_simulation* r){
-	if (reb_output_check(1.)){
-		reb_output_timing();
-	}
-}
-
-void problem_finish(){
+	reb_integrate(r, INFINITY);
 }
