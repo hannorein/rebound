856ce74cf28e0d468b397d3edc407adb7290df44
Hanno Rein <hanno@hanno-rein.de>
2015-07-23 11:09:21 -0400

    two more examples

--- python_examples/megno_simple/problem.py
+++ python_examples/megno_simple/problem.py
@@ -10,21 +10,21 @@ import numpy as np
 def simulation(integrator):
     print("Running "+integrator)
     with open(integrator+".txt","w") as f:
-        rebound.reset()
-        rebound.integrator = integrator
-        rebound.dt = 0.2
+        sim = rebound.Simulation()
+        sim.integrator = integrator
+        sim.dt = 0.2
             
-        rebound.add(m=1.)
-        rebound.add(m=0.01, a=1,e=0.1)
-        rebound.add(m=0.01, a=2.)
+        sim.add(m=1.)
+        sim.add(m=0.01, a=1,e=0.1)
+        sim.add(m=0.01, a=2.)
 
-        rebound.move_to_com()
-        rebound.init_megno(1e-10)
-        particles = rebound.particles
+        sim.move_to_com()
+        sim.init_megno(1e-10)
+        particles = sim.particles
         times = np.logspace(2,5,num=1000)
         for t in times:
-            rebound.integrate(t,0)
-            print("%e %e %e %e %e %e %e %e\n" %(rebound.t, rebound.calculate_megno(), particles[0].x, particles[1].x, particles[2].x, particles[3].x, particles[4].x, particles[5].x),file=f)
+            sim.integrate(t,0)
+            print("%e %e %e %e %e %e %e %e\n" %(sim.t, sim.calculate_megno(), particles[0].x, particles[1].x, particles[2].x, particles[3].x, particles[4].x, particles[5].x),file=f)
 
 simulation("ias15")
 simulation("whfast")
