3b1f51be558f38ca1c71ade7d9684c39fdf59667
Hanno Rein <hanno@hanno-rein.de>
2015-07-23 11:00:06 -0400

    removed two rather long and probably not very useful examples

--- python_examples/dragforce/problem.py
+++ python_examples/dragforce/problem.py
@@ -3,19 +3,20 @@ import rebound
 
 # Add particles
 # We work in units where G=1.  
-rebound.add(m=1. )                  # Test particle
-rebound.add(m=1e-3,x=1.,vy=1. )     # Planet
+sim = rebound.Simulation()
+sim.add(m=1. )                  # Test particle
+sim.add(m=1e-3,x=1.,vy=1. )     # Planet
 
 # Move particles so that the center of mass is (and stays) at the origin  
-rebound.move_to_com()
+sim.move_to_com()
 
 # You can provide a function, written in python to REBOUND.
 # This function gets called every time the forces are evaluated.
 # Simple add any any additional (non-gravitational) forces to the 
 # particle accelerations. Here, we add a simple drag force. This 
 # will make the planet spiral into the star.
-ps = rebound.particles
-def dragforce():
+ps = sim.particles
+def dragforce(reb_sim):
     dragcoefficient = 1e-2
     for p in ps:
         p.ax += -dragcoefficient * p.vx
@@ -23,10 +24,10 @@ def dragforce():
         p.az += -dragcoefficient * p.vz
 
 # Tell rebound which function to call
-rebound.additional_forces = dragforce
+sim.additional_forces = dragforce
 
 # Integrate until t=100 (roughly 16 orbits at 1 AU) 
-rebound.integrate(100.)
+sim.integrate(100.)
 
 # Output something at the end (the planet will be at ~0.1 AU)
 for p in ps:
