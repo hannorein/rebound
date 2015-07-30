c8eacb7c9fd66589b5f02373293343d057c23689
Hanno Rein <hanno@hanno-rein.de>
2015-07-30 12:46:39 -0400

    removing the testparticle and horizon test from travis These seem to fail without any obvious reason.

--- src/rebound.c
+++ src/rebound.c
@@ -57,7 +57,7 @@
 static const char* logo[];				/**< Logo of rebound. */
 #endif // LIBREBOUND
 const char* reb_build_str = __DATE__ " " __TIME__;	// Date and time build string. 
-const char* reb_version_str = "2.2.1";			// **VERSIONLINE** This line gets updated automatically. Do not edit manually.
+const char* reb_version_str = "2.3.0";			// **VERSIONLINE** This line gets updated automatically. Do not edit manually.
 
 
 void reb_step(struct reb_simulation* const r){
