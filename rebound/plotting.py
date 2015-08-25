b84727ab9ef534d804494dd275e14e5200c55b70
Hanno Rein <hanno@hanno-rein.de>
2015-08-25 14:45:55 -0400

    Orbitplot tests

--- rebound/plotting.py
+++ rebound/plotting.py
@@ -40,24 +40,25 @@ def OrbitPlot(sim, figsize=(5,5), lim=None, Narc=50, unitlabel=None, color=False
 
 
         phase = np.linspace(0,2.*np.pi,Narc)
+        ax.scatter([0.],[0.], marker="x", s=25, facecolor="black", edgecolor=None, zorder=3)
         for i, o in enumerate(orbits):
             primary = sim.calculate_com(i+1)
             colori = cm(float(i+1)/float(sim.N-1))
-            pp = Particle(a=o.a, anom=o.f, MEAN=False, inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
-            ax.scatter(pp.x, pp.y, s=25, facecolor="black", edgecolor=None, zorder=2)
+            pp = Particle(a=o.a, f=o.f, inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
+            ax.scatter(pp.x, pp.y, s=25, facecolor="black", edgecolor=None, zorder=3)
             for ph in phase:
-                newp = Particle(a=o.a, anom=o.f+ph, MEAN=False, inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
+                newp = Particle(a=o.a, f=o.f+ph, inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
                 if trails:
                     alpha = ph/(2.*np.pi)
                     color = (colori[0], colori[1], colori[2], alpha)
                 else:
                     color = colori
-                ax.plot([pp.x, newp.x], [pp.y, newp.y], color=color, zorder=1)
+                ax.plot([pp.x, newp.x], [pp.y, newp.y], color=color, zorder=2)
                 pp = newp
             
             if showPeriastron:
-                newp = Particle(a=o.a, anom=0., MEAN=False, inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
-                ax.plot([primary.x, newp.x], [primary.y, newp.y], linestyle="dotted", c=colori, zorder=0)
+                newp = Particle(a=o.a, f=0., inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
+                ax.plot([primary.x, newp.x], [primary.y, newp.y], linestyle="dotted", c=colori, zorder=1)
 
 
 
