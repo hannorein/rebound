abab5b06206af836f6079db75a8297530a7a21cd
Hanno Rein <hanno@hanno-rein.de>
2015-09-25 15:56:18 -0400

    version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='rebound',
-    version='2.7.3',
+    version='2.8.0',
     description='An open-source multi-purpose N-body code',
     long_description=long_description,
     url='http://github.com/hannorein/rebound',
