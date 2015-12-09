d544930e4f6f19566fe60ef1b64fffd0c065ae51
r-cloutier <cloutier@astro.utoronto.ca>
2015-12-09 12:14:38 -0500

    Create problem.py

---
+++ python_examples/satellite_evolution/problem.py
@@ -0,0 +1,8 @@
+'''
+Ryan Cloutier
+
+In this example, we initialize a sparse circumplanetary disk of satellites 
+around a planet orbiting the Sun. The initial outer edge of the disk extends 
+to the Hill radius of the planet. We integrate to study the evolution of 
+satellites under the influence of the planet and solar gravitational potentials.
+'''
