6dba0d792a05915d9c8e00276b66f2c60a1cfdd9
Hanno Rein <hanno@hanno-rein.de>
2016-01-11 10:36:15 -0500

    test 3

---
+++ examples/planetesimals3/problem.c
@@ -0,0 +1,82 @@
+/**
+ * A.S. This is my planetesimal disk with special integrator for close encounters.
+ *      Particle id's: 0 = star, 1 = massive body, 2 = planetesimal, 3 = CLOSE ENCOUNTER
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include "rebound.h"
+
+void heartbeat(struct reb_simulation* r);
+
+int main(int argc, char* argv[]){
+    struct reb_simulation* r = reb_create_simulation();
+
+    int N_planetesimals = 100;
+    double planetesimal_mass = 3e-8;                               //each is a moon
+    double amin = 0.59, amax = 0.61;  //for planetesimal disk
+    double powerlaw = 0.5;
+    
+	//Simulation Setup
+	r->integrator	= REB_INTEGRATOR_IAS15;
+	r->heartbeat	= heartbeat;
+    r->dt = 0.1;
+    r->usleep = 50000;
+    
+	// Initial conditions
+	struct reb_particle star = {0};
+	star.m 		= 1;
+	star.r		= 0.005;        // Radius of particle is in AU!
+    star.id     = 0;            // 0 = star
+	reb_add(r, star);
+    
+    
+    //planet 1
+    {
+        double a=0.5, m=5e-4, e=0, inc = reb_random_normal(0.00001);
+        struct reb_particle p1 = {0};
+        p1 = reb_tools_orbit_to_particle(r->G, star, m, a, e, inc, 0, 0, 0);
+        p1.r = 1.6e-4;              //radius of particle is in AU!
+        p1.id = r->N;
+        reb_add(r, p1);
+    }
+    
+    //planet 2
+    {
+        double a=0.7, m=5e-4, e=0.01, inc=reb_random_normal(0.00001);
+        struct reb_particle p2 = {0};
+        p2 = reb_tools_orbit_to_particle(r->G, star, m, a, e, inc, 0, 0, 0);
+        p2.r = 1.6e-4;
+        p2.id = r->N;
+        reb_add(r, p2);
+    }
+    
+    r->N_active = r->N;
+    reb_move_to_com(r);
+    
+    //planetesimals
+    while(r->N<N_planetesimals + r->N_active){
+		struct reb_particle pt = {0};
+		double a	= reb_random_powerlaw(amin,amax,powerlaw);
+        double phi 	= reb_random_uniform(0,2.*M_PI);
+        double inc = reb_random_normal(0.0001);
+        double Omega = reb_random_uniform(0,2.*M_PI);
+        double apsis = reb_random_uniform(0,2.*M_PI);
+        pt = reb_tools_orbit_to_particle(r->G, star, 0., a, 0., inc, Omega, apsis,phi);
+		pt.r 		= 4e-5;
+        pt.id       = r->N;
+		reb_add(r, pt);
+	}
+    
+    //Integrate!
+    reb_integrate(r, 100.);
+}
+
+void heartbeat(struct reb_simulation* r){
+    reb_output_timing(r, 0);  
+}
+
