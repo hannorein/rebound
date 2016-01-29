bc5e5b5c10a33a9f2ee01e9e9ba1d7f92af1358c
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-01-29 13:58:03 -0500

    fixed typo in rebound.h and updated add_exit_cond.py

--- add_exit_cond.py
+++ add_exit_cond.py
@@ -23,7 +23,7 @@ with open("src/rebound.h", "w") as f:
         if "struct reb_simulation {" in rebh[i]:
             f.write("    {0} {1};\n".format(param_type_c, param_name))
         if "enum REB_STATUS {" in rebh[i]:
-            f.write("REB_{0} = {1},\n".format(param_name.upper(), param_enum_code))
+            f.write("    REB_{0} = {1},\n".format(param_name.upper(), param_enum_code))
 
 with open("src/rebound.c") as f:
     rebc = f.readlines()
@@ -37,18 +37,21 @@ with open("src/rebound.c", "w") as f:
             f.write("        if (fill_in_here){\n")
             f.write("            r->status = REB_{0};\n".format(param_name.upper()))
             f.write("        }\n")
-            f.write("    }")
+            f.write("    }\n")
+        if "void reb_init_simulation(" in rebc[i]:
+            f.write("    r->{0} = 0;\n".format(param_name))
 
 with open("rebound/simulation.py") as f:
     sim = f.readlines()
 
 with open("rebound/simulation.py", "w") as f:
     for i in range(len(sim)):
-        f.write(sim[i])
+        if "from . import clibrebound" in sim[i]:
+            f.write(sim[i].rstrip()+", {0}\n".format(exception))
+        else:
+            f.write(sim[i])
         if "Simulation._fields_ = " in sim[i]:
             f.write('\t\t\t\t("{0}", {1}),\n'.format(param_name, param_ctype))
-        if "from . import clibrebound" in sim[i]:
-            f.write(sim[i]+", {0}\n".format(exception))
         if "ret_value = clibrebound.reb_integrate" in sim[i]:
             f.write("\t\t\tif ret_value == {0}:\n".format(param_enum_code))
             f.write('\t\t\t\traise {0}("{1}")\n'.format(exception, description))
@@ -58,9 +61,11 @@ with open("rebound/__init__.py") as f:
 
 with open("rebound/__init__.py", "w") as f:
     for i in range(len(init)):
-        f.write(init[i])
+        if "__all__ = " in init[i]:
+            init[i] = init[i].rstrip()
+            f.write(init[i][:-1] + ', "{0}"]\n'.format(exception))
+        else:
+            f.write(init[i])
         if "# Exceptions" in init[i]:
             f.write("class {0}(Exception):\n".format(exception))
             f.write("\tpass\n")
-        if "__all__ = " in init[i]:
-            f.write(init[i][:-1] + ', "{0}"]\n'.format(exception))
