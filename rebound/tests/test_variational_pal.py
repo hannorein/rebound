5f330b620f2d0e313a5e7e09118f054d8e37d35a
Hanno Rein <hanno@hanno-rein.de>
2016-04-06 20:49:52 -0400

    Second order tests added (Currently fail)

--- rebound/tests/test_variational_pal.py
+++ rebound/tests/test_variational_pal.py
@@ -2,19 +2,32 @@ import rebound
 import unittest
 import datetime
 
+vlist = ["m","a","h","k","l","ix","iy"]
+paramslist = [ 
+        [1e-3,      1.,     0.,     0.,     0.,         0.0,     0.0],
+        [1e-3,      1.,     0.1,    0.02,   0.3,        0.0,     0.0],
+        [1e-6,      2.,     0.02,   0.0132, 0.33,       0.126,   0.14],
+        [234.3e-6,  1.7567, 0.561,  0.572,  0.573,      0.056,   0.0091354],
+        [1e-2,      1.7567, 0.1561, 0.15472,0.24573,    0.0056,  0.0013],
+        [1e-7,      3.7567, 0.00061,0.23572,0.523473,   0.47256, 0.000024],
+        [1e-7,      3.7567, 0.00061,0.23572,0.523473,   1.99, 0.0],
+        ]
 
 class TestVariationalPal(unittest.TestCase):
-    def test_all_1st_order_full(self):
+
+    def get_sim(self, params):
+        args = dict(zip(vlist,params))
+        sim = rebound.Simulation()
+        sim.add(m=1.)
+        p = rebound.Particle(simulation=sim, primary=sim.particles[0], **args)
+        sim.add(p)
+        sim.add(primary=sim.particles[0],a=1.76)
+        sim.integrate(10.)
+        return sim
+
+
+    def test_all_1st_order(self):
         Delta = 1e-8
-        vlist = ["m","a","h","k","l","ix","iy"]
-        paramslist = [ 
-                [1e-3,      1.,     0.1,    0.02,   0.3,        0.0,     0.0],
-                [1e-6,      2.,     0.02,   0.0132, 0.33,       0.126,   0.14],
-                [234.3e-6,  1.7567, 0.561,  0.572,  0.573,      0.056,   0.0091354],
-                [1e-2,      1.7567, 0.1561, 0.15472,0.24573,    0.0056,  0.0013],
-                [1e-7,      3.7567, 0.00061,0.23572,0.523473,   0.47256, 0.000024],
-                [1e-7,      3.7567, 0.00061,0.23572,0.523473,   2., 0.0],
-                ]
         for params in paramslist:
             for vi,v in enumerate(vlist):
                 args = dict(zip(vlist,params))
@@ -28,13 +41,7 @@ class TestVariationalPal(unittest.TestCase):
                 simvp.integrate(10.)
 
                 params[vi] += Delta
-                args = dict(zip(vlist,params))
-                simsp = rebound.Simulation()
-                simsp.add(m=1.)
-                p = rebound.Particle(simulation=simsp, primary=simsp.particles[0], **args)
-                simsp.add(p)
-                simsp.add(primary=simsp.particles[0],a=1.76)
-                simsp.integrate(10.)
+                simsp = self.get_sim(params)
                 params[vi] -= Delta
 
                 vp = var_i.particles[1]
@@ -44,13 +51,77 @@ class TestVariationalPal(unittest.TestCase):
                 p_fd = (sp-p)/Delta
 
                 prec = 1e-5
-                self.assertLess(abs(p_fd.x -vp.x ), prec, msg="Problem with '%s' derivative for the following parameters: %s."%(v,args))
-                self.assertLess(abs(p_fd.y -vp.y ), prec, msg="Problem with '%s' derivative for the following parameters: %s."%(v,args))
-                self.assertLess(abs(p_fd.z -vp.z ), prec, msg="Problem with '%s' derivative for the following parameters: %s."%(v,args))
-                self.assertLess(abs(p_fd.vx-vp.vx), prec, msg="Problem with '%s' derivative for the following parameters: %s."%(v,args))
-                self.assertLess(abs(p_fd.vy-vp.vy), prec, msg="Problem with '%s' derivative for the following parameters: %s."%(v,args))
-                self.assertLess(abs(p_fd.vz-vp.vz), prec, msg="Problem with '%s' derivative for the following parameters: %s."%(v,args))
-                self.assertLess(abs(p_fd.m -vp.m ), prec, msg="Problem with '%s' derivative for the following parameters: %s."%(v,args))
+                msg = "Problem with '%s' derivative for the following parameters: %s." % (v,args)
+                self.assertLess(abs(p_fd.x -vp.x ), prec, msg=msg)
+                self.assertLess(abs(p_fd.y -vp.y ), prec, msg=msg)
+                self.assertLess(abs(p_fd.z -vp.z ), prec, msg=msg)
+                self.assertLess(abs(p_fd.vx-vp.vx), prec, msg=msg)
+                self.assertLess(abs(p_fd.vy-vp.vy), prec, msg=msg)
+                self.assertLess(abs(p_fd.vz-vp.vz), prec, msg=msg)
+                self.assertLess(abs(p_fd.m -vp.m ), prec, msg=msg)
+    
+    def test_all_2nd_order(self):
+        Delta = 1e-6
+        for params in paramslist:
+            for vi,v in enumerate(vlist):
+                for wi,w in enumerate(vlist):
+                    args = dict(zip(vlist,params))
+                    simvp = rebound.Simulation()
+                    simvp.add(m=1.)
+                    p = rebound.Particle(simulation=simvp, primary=simvp.particles[0], **args)
+                    simvp.add(p)
+                    simvp.add(primary=simvp.particles[0],a=1.76)
+                    var_v = simvp.add_variation()
+                    var_v.vary_pal(1,v)
+                    var_w = simvp.add_variation()
+                    var_w.vary_pal(1,w)
+                    var_vw = simvp.add_variation(order=2, first_order=var_w, first_order_2=var_v)
+                    var_vw.vary_pal(1,w,v)
+                    simvp.integrate(10.)
+
+                    params[vi] += Delta
+                    params[wi] += Delta
+                    simspp = self.get_sim(params)
+                    params[vi] -= Delta
+                    params[wi] -= Delta
+
+                    params[vi] += Delta
+                    params[wi] -= Delta
+                    simspm = self.get_sim(params)
+                    params[vi] -= Delta
+                    params[wi] += Delta
+                    
+                    params[vi] -= Delta
+                    params[wi] += Delta
+                    simsmp = self.get_sim(params)
+                    params[vi] += Delta
+                    params[wi] -= Delta
+                    
+                    params[vi] -= Delta
+                    params[wi] -= Delta
+                    simsmm = self.get_sim(params)
+                    params[vi] += Delta
+                    params[wi] += Delta
+
+                    vp  = var_vw.particles[1]
+                    p   = simvp.particles[1]
+                    spp = simspp.particles[1]
+                    spm = simspm.particles[1]
+                    smp = simsmp.particles[1]
+                    smm = simsmm.particles[1]
+
+                    p_fd = (spp-spm-smp+smm)/(4.*Delta**2)
+
+                    prec = 1e-4
+                    msg = "Problem with '%s_%s' derivative for the following parameters: %s." % (v,w,args)
+                    self.assertLess(abs(p_fd.x -vp.x ), prec, msg=msg)
+                    self.assertLess(abs(p_fd.y -vp.y ), prec, msg=msg)
+                    self.assertLess(abs(p_fd.z -vp.z ), prec, msg=msg)
+                    self.assertLess(abs(p_fd.vx-vp.vx), prec, msg=msg)
+                    self.assertLess(abs(p_fd.vy-vp.vy), prec, msg=msg)
+                    self.assertLess(abs(p_fd.vz-vp.vz), prec, msg=msg)
+                    self.assertLess(abs(p_fd.m -vp.m ), prec, msg=msg)
+
     
     
     
