61ef2f8603f97b587a173ce4c4397a1a9807e925
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-06-02 16:10:00 -0400

    added tools.py

---
+++ rebound/tools.py
@@ -0,0 +1,6 @@
+from ctypes import c_uint32, c_char_p
+from . import clibrebound
+
+def hash(string):
+    clibrebound.reb_hash.restype = c_uint32
+    return clibrebound.reb_hash(c_char_p(string.encode('utf-8')))
