0c432b023b63beec25c3ead8a78561f94205a49c
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-06-07 15:16:26 -0400

    updated mpi and ipython examples

--- src/communication_mpi.c
+++ src/communication_mpi.c
@@ -45,7 +45,6 @@
 #include "tree.h"
 #include "boundary.h"
 #include "communication_mpi.h"
-#include "mpi.h"
 
 void reb_communication_mpi_init(struct reb_simulation* const r, int argc, char** argv){
 	MPI_Init(&argc,&argv);
@@ -75,7 +74,7 @@ void reb_communication_mpi_init(struct reb_simulation* const r, int argc, char**
         struct reb_particle p;
         blen[bnum] 	= 1; 
         indices[bnum] 	= (char*)&p.hash - (char*)&p; 
-        oldtypes[bnum] 	= mpi_uint32_t; //MPI_UINT32_T;
+        oldtypes[bnum] 	= MPI_UINT; // hash is a uint32_t, but not all MPI headers seem to have MPI_UINT32_T
     }
 	bnum++;
     {
