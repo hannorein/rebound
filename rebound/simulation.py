676cf81a08f6e7d85ddbb3e821fec4fadc066bae
Hanno Rein <hanno@hanno-rein.de>
2016-06-17 18:17:02 -0400

    Added pointer for Hermes Global simulation

--- rebound/simulation.py
+++ rebound/simulation.py
@@ -1198,7 +1198,7 @@ class Variation(Structure):
 
 class reb_simulation_integrator_hermes(Structure):
     _fields_ = [("mini", POINTER(Simulation)),
-                ("global", c_void_p),
+                ("global", POINTER(Simulation)),
                 ("hill_switch_factor", c_double),
                 ("radius_switch_factor", c_double),
                 ("mini_active", c_int),
