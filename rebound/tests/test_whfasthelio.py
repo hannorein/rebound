d0b24728a828c9509817cd6057c7d0a65eef0aea
Hanno Rein <hanno@hanno-rein.de>
2016-08-07 13:19:26 -0400

    order test improved

--- rebound/tests/test_whfasthelio.py
+++ rebound/tests/test_whfasthelio.py
@@ -47,23 +47,23 @@ class TestIntegratorWHFastHelio(unittest.TestCase):
 
         sim1 = rebound.Simulation()
         sim1.add(m=1.)
-        sim1.add(m=1e-3, a=1.,e=.1,primary=sim1.particles[0])
+        sim1.add(m=1e-3, a=1.,e=0.1,primary=sim1.particles[0])
         sim1.add(m=1e-3, a=3.,e=0.1,primary=sim1.particles[0])
         sim1.integrator = "whfasthelio"
         sim1.dt = 0.005123*jupyr
-        sim1.integrate(1e3*jupyr)
+        sim1.integrate(1e0*jupyr)
         sim2 = rebound.Simulation()
         sim2.add(m=1.)
         sim2.add(m=1e-3, a=3.,e=0.1,primary=sim2.particles[0])
-        sim2.add(m=1e-3, a=1.,e=.1,primary=sim2.particles[0])
+        sim2.add(m=1e-3, a=1.,e=0.1,primary=sim2.particles[0])
         sim2.integrator = "whfasthelio"
         sim2.dt = 0.005123*jupyr
-        sim2.integrate(1e3*jupyr)
+        sim2.integrate(1e0*jupyr)
         
-        self.assertAlmostEqual(sim1.particles[1].x, sim2.particles[2].x, delta=1e-9)
-        self.assertAlmostEqual(sim1.particles[1].y, sim2.particles[2].y, delta=1e-9)
-        self.assertAlmostEqual(sim1.particles[2].x, sim2.particles[1].x, delta=1e-9)
-        self.assertAlmostEqual(sim1.particles[2].y, sim2.particles[1].y, delta=1e-9)
+        self.assertAlmostEqual(sim1.particles[1].x, sim2.particles[2].x, delta=1e-15)
+        self.assertAlmostEqual(sim1.particles[1].y, sim2.particles[2].y, delta=1e-15)
+        self.assertAlmostEqual(sim1.particles[2].x, sim2.particles[1].x, delta=1e-15)
+        self.assertAlmostEqual(sim1.particles[2].y, sim2.particles[1].y, delta=1e-15)
         
     
     def test_whfasthelio_nosafemode(self):
