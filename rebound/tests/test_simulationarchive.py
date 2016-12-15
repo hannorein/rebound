303eb07f27439ec6cfc56081c226a24d24511b0a
Hanno Rein <hanno@hanno-rein.de>
2016-12-14 20:06:17 -0500

    A memory leak issue existed for from_archive(). This fixes it by allocating the memory in python using stypes which sets the _b_needsfree parameter to 1. Added unit test.

--- rebound/tests/test_simulationarchive.py
+++ rebound/tests/test_simulationarchive.py
@@ -3,6 +3,34 @@ import unittest
 import warnings
 
 class TestSimulationArchive(unittest.TestCase):
+    def test_sa_from_archive(self):
+        sim = rebound.Simulation()
+        sim.add(m=1)
+        sim.add(m=1e-3,a=1,e=0.1,omega=0.1,M=0.1,inc=0.1,Omega=0.1)
+        sim.add(m=1e-3,a=-2,e=1.1,omega=0.1,M=0.1,inc=0.1,Omega=0.1)
+        sim.integrator = "whfast"
+        sim.dt = 0.1313
+        sim.ri_whfast.safe_mode = 1
+        sim.initSimulationArchive("test.bin", 10.)
+        sim.integrate(40.,exact_finish_time=0)
+
+        sim = None
+        sim = rebound.Simulation.from_archive("test.bin")
+        sim.integrate(80.,exact_finish_time=0)
+        x1 = sim.particles[1].x
+        
+        
+        sim = rebound.Simulation()
+        sim.add(m=1)
+        sim.add(m=1e-3,a=1,e=0.1,omega=0.1,M=0.1,inc=0.1,Omega=0.1)
+        sim.add(m=1e-3,a=-2,e=1.1,omega=0.1,M=0.1,inc=0.1,Omega=0.1)
+        sim.integrator = "whfast"
+        sim.dt = 0.1313
+        sim.ri_whfast.safe_mode = 1
+        sim.integrate(80.,exact_finish_time=0)
+        x0 = sim.particles[1].x
+
+        self.assertEqual(x0,x1)
     def test_safe_mode_warning(self):
         sim = rebound.Simulation()
         sim.add(m=1)
