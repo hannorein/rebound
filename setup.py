896b7875fdde3d9f0b4a55640f9d069d88501766
Hanno Rein <hanno@hanno-rein.de>
2017-01-19 11:29:44 -0500

    Updating version to 3.2.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ try:
     ghash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii")
     ghash_arg = "-DGITHASH="+ghash.strip()
 except:
-    ghash_arg = "-DGITHASH=075ea64d2d830b825eb39a3d45fe945a34a4240d" #GITHASHAUTOUPDATE
+    ghash_arg = "-DGITHASH=89622b3253fcc9a044cd545b008a1cf91fba9d6f" #GITHASHAUTOUPDATE
 
 extra_link_args=[]
 if sys.platform == 'darwin':
@@ -58,7 +58,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='rebound',
-    version='3.2.0',
+    version='3.2.1',
     description='An open-source multi-purpose N-body code',
     long_description=long_description,
     url='http://github.com/hannorein/rebound',
