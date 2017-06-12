96e37577daaf67e1cc06b6c8f50b5d24e98ca354
Daniel Tamayo <tamayo.daniel@gmail.com>
2017-06-12 15:51:18 -0400

    added tok

--- triggerdependencybuilds.sh
+++ triggerdependencybuilds.sh
@@ -5,12 +5,10 @@ body='{
 "branch":"master"
 }}'
 
-echo $TRAVIS_API_TOKEN
-
 curl -s -X POST \
    -H "Content-Type: application/json" \
    -H "Accept: application/json" \
    -H "Travis-API-Version: 3" \
-   -H "Authorization: token $TRAVIS_API_TOKEN" \
+   -H "Authorization: token "$TRAVIS_API_TOKEN \
    -d "$body" \
    https://api.travis-ci.org/repo/dtamayo%2Freboundx/requests
