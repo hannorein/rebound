911279987b13e0520a2943282273cccf928d916f
Hanno Rein <hanno@hanno-rein.de>
2017-08-30 11:50:29 -0400

    spaces vs tabs

--- examples/solar_system_with_testparticles/problem.c
+++ examples/solar_system_with_testparticles/problem.c
@@ -18,30 +18,30 @@
 void heartbeat(struct reb_simulation* r);
 
 int main(int argc, char* argv[]){
-	struct reb_simulation* r = reb_create_simulation_from_binary("ss-2016-06-18.bin");
-	// Setup constants
-	r->dt 			= 4./365.25*2.*M_PI;	    // 4days
-	r->integrator	= REB_INTEGRATOR_WHFAST;
+    struct reb_simulation* r = reb_create_simulation_from_binary("ss-2016-06-18.bin");
+    // Setup constants
+    r->dt           = 4./365.25*2.*M_PI;        // 4days
+    r->integrator   = REB_INTEGRATOR_WHFAST;
     r->ri_whfast.coordinates   = REB_WHFAST_COORDINATES_DEMOCRATICHELIOCENTRIC;
-	r->heartbeat	= heartbeat;
+    r->heartbeat    = heartbeat;
     r->N_active     = r->N;
 
-	// Test Particles
-	for (int i=0;i<10000;i++){
+    // Test Particles
+    for (int i=0;i<10000;i++){
         double a = reb_random_uniform(0.4,20.);
         double e = reb_random_uniform(0.01,0.2);
         double omega = reb_random_uniform(0.,2.*M_PI);
         double f = reb_random_uniform(0.,2.*M_PI);
-		struct reb_particle p = reb_tools_orbit2d_to_particle(1.,r->particles[0],0.,a,e,omega,f);
-		reb_add(r, p); 
-	}
-	reb_move_to_com(r);
-	reb_integrate(r, INFINITY);
+        struct reb_particle p = reb_tools_orbit2d_to_particle(1.,r->particles[0],0.,a,e,omega,f);
+        reb_add(r, p); 
+    }
+    reb_move_to_com(r);
+    reb_integrate(r, INFINITY);
 }
 
 void heartbeat(struct reb_simulation* r){
-	if (reb_output_check(r, 100.)){
-		reb_output_timing(r, INFINITY);
-	}
+    if (reb_output_check(r, 100.)){
+        reb_output_timing(r, INFINITY);
+    }
 }
 
