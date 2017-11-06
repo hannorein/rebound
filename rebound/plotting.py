9bdcedfaead71bdb832c957354e869502ea1f14c
Daniel Tamayo <tamayo.daniel@gmail.com>
2017-11-05 23:04:41 -0500

    refactored plotting

--- rebound/plotting.py
+++ rebound/plotting.py
@@ -84,16 +84,76 @@ def OrbitPlot(sim, figsize=None, lim=None, limz=None, Narc=100, unitlabel=None, 
         OrbitPlotOneSlice(sim, ax, lim=lim, Narc=Narc, color=color, periastron=periastron, trails=trails, show_orbit=show_orbit, lw=lw, plotparticles=plotparticles)
     return fig
 
-def getcolor(color):
-    import matplotlib.colors as mplcolors
+def get_color(color):
+    """
+    Takes a string for a color name defined in matplotlib and returns of a 3-tuple of RGB values.
+    Will simply return passed value if it's a tuple of length three.
+
+    Parameters
+    ----------
+    color   : str
+        Name of matplotlib color to calculate RGB values for.
+    """
+
+    if isinstance(color, tuple) and len(color) == 3: # already a tuple of RGB values
+        return color
+
+    try:
+        import matplotlib.colors as mplcolors
+    except:
+        raise ImportError("Error importing matplotlib. If running from within a jupyter notebook, try calling '%matplotlib inline' beforehand.")
+   
     try:
         hexcolor = mplcolors.cnames[color]
     except KeyError:
-        raise AttributeError("Color passed to OrbitPlot not recognized in matplotlib.")
+        raise AttributeError("Color not recognized in matplotlib.")
 
     hexcolor = hexcolor.lstrip('#')
     lv = len(hexcolor)
     return tuple(int(hexcolor[i:i + lv // 3], 16)/255. for i in range(0, lv, lv // 3)) # tuple of rgb values
+
+def fading_line(x, y, color='black', alpha_initial=1., alpha_final=0., **kwargs):
+    """
+    Returns a matplotlib LineCollection connecting the points in the x and y lists, with a single color and alpha varying from alpha_initial to alpha_final along the line.
+    Can pass any kwargs you can pass to LineCollection, like linewidgth.
+
+    Parameters
+    ----------
+    x       : list or array of floats for the positions on the (plot's) x axis
+    y       : list or array of floats for the positions on the (plot's) y axis
+    color   : matplotlib color for the line. Can also pass a 3-tuple of RGB values (default: 'black')
+    alpha_initial:  Limiting value of alpha to use at the beginning of the arrays.
+    alpha_final:    Limiting value of alpha to use at the end of the arrays.
+    """
+    try:
+        from matplotlib.collections import LineCollection
+        from matplotlib.colors import LinearSegmentedColormap
+        import numpy as np
+    except:
+        raise ImportError("Error importing matplotlib and/or numpy. Plotting functions not available. If running from within a jupyter notebook, try calling '%matplotlib inline' beforehand.")
+
+    color = get_color(color)
+    cdict = {'red': ((0.,color[0],color[0]),(1.,color[0],color[0])),
+             'green': ((0.,color[1],color[1]),(1.,color[1],color[1])),
+             'blue': ((0.,color[2],color[2]),(1.,color[2],color[2])),
+             'alpha': ((0.,alpha_initial, alpha_initial), (1., alpha_final, alpha_final))}
+    
+    Npts = len(x)
+    if len(y) != Npts:
+        raise AttributeError("x and y must have same dimension.")
+   
+    segments = np.zeros((Npts-1,2,2))
+    segments[0][0] = [x[0], y[0]]
+    for i in range(1,Npts-1):
+        pt = [x[i], y[i]]
+        segments[i-1][1] = pt
+        segments[i][0] = pt 
+    segments[-1][1] = [x[-1], y[-1]]
+
+    individual_cm = LinearSegmentedColormap('indv1', cdict)
+    lc = LineCollection(segments, cmap=individual_cm, **kwargs)
+    lc.set_array(np.linspace(0.,1.,len(segments)))
+    return lc
 
 def OrbitPlotOneSlice(sim, ax, lim=None, limz=None, Narc=100, color=False, periastron=False, trails=False, show_orbit=True, lw=1., axes="xy", plotparticles=[]):
     import matplotlib.pyplot as plt
@@ -101,34 +161,30 @@ def OrbitPlotOneSlice(sim, ax, lim=None, limz=None, Narc=100, color=False, peria
     from matplotlib.colors import LinearSegmentedColormap
     import numpy as np
     if not plotparticles: # default. No plotparticles list passed, so plot all obits
-        orbits = sim.calculate_orbits()
+        ps = sim.particles[1:]
     else: 
-        orbits = []
-        for p in plotparticles:
-            try:
-                orbits.append(sim.particles[p].orbit)
-            except ParticleNotFound:
-                raise AttributeError("Particle in plotparticles list passed to OrbitPlot not found in the simulation.")
+        try:
+            ps = [sim.particles[i] for i in plotparticles]
+        except ParticleNotFound:
+            raise AttributeError("Particle in plotparticles list passed to OrbitPlot not found in the simulation.")
 
-    particles = sim.particles
     if lim is None:
         lim = 0.
-        for o in orbits:
-            if o.a>0.:
-                r = (1.+o.e)*o.a
+        for p in ps:
+            if p.a>0.:
+                r = (1.+p.e)*p.a
             else:
-                r = o.d
+                r = p.d
             if r>lim:
                 lim = r
         lim *= 1.15
     if limz is None:
-        z = [p.z for p in particles]
+        z = [p.z for p in ps]
         limz = 2.0*max(z)
         if limz > lim:
             limz = lim
         if limz <= 0.:
             limz = lim
-
 
     if axes[0]=="z":
         ax.set_xlim([-limz,limz])
@@ -143,92 +199,51 @@ def OrbitPlotOneSlice(sim, ax, lim=None, limz=None, Narc=100, color=False, peria
         if color == True:
             colors = [(1.,0.,0.),(0.,0.75,0.75),(0.75,0.,0.75),(0.75, 0.75, 0,),(0., 0., 0.),(0., 0., 1.),(0., 0.5, 0.)]
         if isinstance(color, str):
-            colors = [getcolor(color)]
+            colors = [get_color(color)]
         if isinstance(color, list):
             colors = []
             for c in color:
-                colors.append(getcolor(c))
+                colors.append(get_color(c))
     else:
-        colors = [(0.,0.,0.)]
-    
+        colors = ['black']
     coloriterator = cycle(colors)
 
-    ax.scatter(getattr(particles[0],axes[0]),getattr(particles[0],axes[1]), marker="*", s=35*lw, facecolor="black", edgecolor=None, zorder=3)
-    for i, o in enumerate(orbits):
+    coords = {'x':0, 'y':1, 'z':2}
+    axis0 = coords[axes[0]]
+    axis1 = coords[axes[1]]
+    
+    ax.scatter(getattr(sim.particles[0],axes[0]),getattr(sim.particles[0],axes[1]), marker="*", s=35*lw, facecolor="black", edgecolor=None, zorder=3)
+    
+    proj = {}
+    for p in ps:
+        ax.scatter(getattr(p,axes[0]), getattr(p,axes[1]), s=25*lw, facecolor="black", edgecolor=None, zorder=3)
         colori = next(coloriterator)
-        primary = sim.calculate_com(last=i)
-        pp = Particle(a=o.a, f=o.f, inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
-        ax.scatter(getattr(pp,axes[0]), getattr(pp,axes[1]), s=25*lw, facecolor="black", edgecolor=None, zorder=3)
-        if o.a>0.: # bound orbit
-            segments = np.zeros((Narc,2,2))
-            if show_orbit:
-                phase = np.linspace(0,2.*np.pi,Narc)
-                for j,ph in enumerate(phase):
-                    newp = Particle(a=o.a, f=o.f+ph, inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
-                    xy = [getattr(newp,axes[0]), getattr(newp,axes[1])]
-                    segments[j][1]=xy
-                    segments[(j+1)%Narc][0]=xy
-            cdict = {'red': ((0.,colori[0],colori[0]),(1.,colori[0],colori[0])),
-                     'green': ((0.,colori[1],colori[1]),(1.,colori[1],colori[1])),
-                     'blue': ((0.,colori[2],colori[2]),(1.,colori[2],colori[2]))}
-            if trails:
-                cdict['alpha'] = ((0.,0.,0.),(1.,1.,1.))
-            individual_cm = LinearSegmentedColormap('indv1', cdict)
-            lc = LineCollection(segments, cmap=individual_cm, linewidth=lw)
-            if show_orbit:
-                lc.set_array(phase)
-            ax.add_collection(lc)
-        else:     # unbound orbit.  Step in M rather than f, since for hyperbolic orbits f stays near lim, and jumps to -f at peri
-            t_cross = 4.*o.d/o.v # rough time to cross display box
-            
-            lim_phase = abs(o.n)*t_cross # M = nt, n is negative
-            segments = np.zeros((Narc,2,2))
-            if show_orbit:
-                phase = np.linspace(-lim_phase+o.M,o.M,Narc)
-                for j,ph in enumerate(phase):
-                    newp = Particle(a=o.a, M=ph, inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
-                    xy = [getattr(newp,axes[0]), getattr(newp,axes[1])]
-                    segments[j][1]=xy
-                    if j+1<Narc:
-                       segments[j+1][0]=xy
-                    if j==0:
-                       segments[j][0]=xy
-            cdict = {'red': ((0.,colori[0],colori[0]),(1.,colori[0],colori[0])),
-                     'green': ((0.,colori[1],colori[1]),(1.,colori[1],colori[1])),
-                     'blue': ((0.,colori[2],colori[2]),(1.,colori[2],colori[2]))}
-            if trails:
-                cdict['alpha'] = ((0.,0.,0.),(1.,1.,1.))
-            individual_cm = LinearSegmentedColormap('indv1', cdict)
-            lc = LineCollection(segments, cmap=individual_cm, linewidth=lw)
-            if show_orbit:
-                lc.set_array(phase)
-            ax.add_collection(lc)
-            
-            segments = np.zeros((Narc,2,2))
-            if show_orbit:
-                phase = np.linspace(o.M,o.M+lim_phase,Narc)
-                for j,ph in enumerate(phase):
-                    newp = Particle(a=o.a, M=ph, inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
-                    xy = [getattr(newp,axes[0]), getattr(newp,axes[1])]
-                    segments[j][1]=xy
-                    if j+1<Narc:
-                       segments[j+1][0]=xy
-                    if j==0:
-                       segments[j][0]=xy
-            cdict = {'red': ((0.,colori[0],colori[0]),(1.,colori[0],colori[0])),
-                     'green': ((0.,colori[1],colori[1]),(1.,colori[1],colori[1])),
-                     'blue': ((0.,colori[2],colori[2]),(1.,colori[2],colori[2]))}
-            if trails:
-                cdict['alpha'] = ((0.,0.2,0.2),(1.,0.2,0.2))
-            individual_cm = LinearSegmentedColormap('indv1', cdict)
-            lc = LineCollection(segments, cmap=individual_cm, linewidth=lw)
-            if show_orbit:
-                lc.set_array(phase)
-            ax.add_collection(lc)
-        
-        
+       
+        if show_orbit is True:
+            alpha_final = 0. if trails is True else 1. # fade to 0 with trails
+
+            hyperbolic = p.a < 0. # Boolean for whether orbit is hyperbolic
+            if hyperbolic is False:
+                o = np.array(p.sample_orbit())
+                proj['x'],proj['y'],proj['z'] = [o[:,i] for i in range(3)]
+                lc = fading_line(proj[axes[0]], proj[axes[1]], colori, alpha_final=alpha_final, lw=lw)
+                ax.add_collection(lc)
+            else:
+                o = np.array(p.sample_orbit(useTrueAnomaly=False))
+                # true anomaly stays close to limiting value and switches quickly at pericenter for hyperbolic orbit, so use mean anomaly
+                proj['x'],proj['y'],proj['z'] = [o[:,i] for i in range(3)]
+                lc = fading_line(proj[axes[0]], proj[axes[1]], colori, alpha_final=alpha_final, lw=lw)
+                ax.add_collection(lc)
+          
+                alpha = 0.2 if trails is True else 1.
+                o = np.array(p.sample_orbit(trailing=False, useTrueAnomaly=False))
+                proj['x'],proj['y'],proj['z'] = [o[:,i] for i in range(3)]
+                lc = fading_line(proj[axes[0]], proj[axes[1]], colori, alpha_initial=alpha, alpha_final=alpha, lw=lw)
+                ax.add_collection(lc)
+
         if periastron:
-            newp = Particle(a=o.a, f=0., inc=o.inc, omega=o.omega, Omega=o.Omega, e=o.e, m=particles[i+1].m, primary=primary, simulation=sim)
+            primary = p.jacobi_com
+            newp = Particle(a=p.a, f=0., inc=p.inc, omega=p.omega, Omega=p.Omega, e=p.e, m=p.m, primary=primary, simulation=sim)
             ax.plot([getattr(primary,axes[0]), getattr(newp,axes[0])], [getattr(primary,axes[1]), getattr(newp,axes[1])], linestyle="dotted", c=colori, zorder=1, lw=lw)
             ax.scatter([getattr(newp,axes[0])],[getattr(newp,axes[1])], marker="o", s=5.*lw, facecolor="none", edgecolor=colori, zorder=1)
 
