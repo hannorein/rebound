c51b14c4e59958cb999ead4b693bc7f0d5f10313
Hanno Rein <hanno@hanno-rein.de>
2018-04-10 09:45:43 -0400

    scel

---
+++ src/binarydiff.c
@@ -0,0 +1,40 @@
+/**
+ * @file    binarydiff.c
+ * @brief   Binary diff allows to compare binary snapshots.
+ * @author  Hanno Rein <hanno@hanno-rein.de>
+ * 
+ * @section     LICENSE
+ * Copyright (c) 2018 Hanno Rein
+ *
+ * This file is part of rebound.
+ *
+ * rebound is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rebound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <sys/time.h>
+#include "particle.h"
+#include "rebound.h"
+#include "tools.h"
+#include "binarydiff.h"
+
+
+void reb_output_binary(FILE* f1, FILE* f2){
+
+}
---
+++ src/binarydiff.h
@@ -0,0 +1,28 @@
+/**
+ * @file 	binarydiff.h
+ * @brief 	Binary diff allows to compare binary snapshots.
+ * @author 	Hanno Rein <hanno@hanno-rein.de>
+ * 
+ * @section 	LICENSE
+ * Copyright (c) 2018 Hanno Rein
+ *
+ * This file is part of rebound.
+ *
+ * rebound is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rebound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef _BINARYDIFF_H
+#define _BINARYDIFF_H
+
+#endif // _BINARYDIFF_H
