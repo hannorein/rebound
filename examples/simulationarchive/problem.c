076eadc74d8e83f69df9caf3adf4b4481b5a60d5
Hanno Rein <hanno@hanno-rein.de>
2018-04-28 14:59:20 -0400

    example

--- examples/simulationarchive/problem.c
+++ examples/simulationarchive/problem.c
@@ -21,11 +21,16 @@
 int main(int argc, char* argv[]) {
     char* filename = "simulationarchive.bin";
 
-    // Trying to restart from the Simulation Archive.
+    // Trying to open a SimulationArchive file
     struct reb_simulationarchive* sa = reb_open_simulationarchive(filename);
+    if (sa==NULL){
+        printf("Can not open file.\n");
+    }
+    // Get a simulation from the file (if possible, otherwise NULL is returned)
     struct reb_simulation* r = reb_create_simulation_from_simulationarchive(sa,-1);
-    //reb_close_simulationarchive(sa);
-    // Check if that was successful
+    // Whenever you've opened a SimulationArchive and don't need it anymore, close it.
+    reb_close_simulationarchive(sa);
+    // Check if we were successful
     if (r==NULL){
         printf("No simulation archive found. Creating new simulation.\n");
         r= reb_create_simulation();
@@ -36,8 +41,8 @@ int main(int argc, char* argv[]) {
         struct reb_particle planet2 = reb_tools_orbit2d_to_particle(r->G, star, 1e-3, 2.3, 0.01, 0., 0.);
         reb_add(r, planet2);
         reb_move_to_com(r);
-        r->dt = 6./365.25*2.*M_PI;                      // 6 days in units where G=1 
-        r->ri_whfast.safe_mode = 0;                      
+        r->dt = 6./365.25*2.*M_PI;              // 6 days in units where G=1 
+        r->ri_whfast.safe_mode = 0;             // The SimulationArchive works with both safe_mode on and off           
         r->ri_whfast.corrector = 5;    
         r->integrator = REB_INTEGRATOR_WHFAST;
     }else{
@@ -46,11 +51,14 @@ int main(int argc, char* argv[]) {
     
     // Automatically create a snapshot every 100 time units
     reb_simulationarchive_automate_interval(r,filename,100.);
+    // Alternatively, you can also create a snapshot every 5 seconds (walltime)
+    //reb_simulationarchive_automate_walltime(r,filename,5.);
 
-    reb_integrate(r, r->t+2000); // integrate a little further
+    // Run the integration (this will be very quick in this example)
+    reb_integrate(r, r->t+2000); // integrate (a little further than where we currently are)
     printf("Final time: %f\n",r->t);
     
-    // Manually append a snapshot
+    // You can also manually append a snapshot
     reb_simulationarchive_snapshot(r,filename);
 }
 
