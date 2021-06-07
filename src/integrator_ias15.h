697c25396ceb9ba05908d5b5bc95e2533b058165
Hanno Rein <hanno@hanno-rein.de>
2021-06-07 11:10:21 -0400

    Moved function declarations for REBOUNDx. (temporary fix)

--- src/integrator_ias15.h
+++ src/integrator_ias15.h
@@ -28,6 +28,4 @@ void reb_integrator_ias15_part1(struct reb_simulation* r);         ///< Internal
 void reb_integrator_ias15_synchronize(struct reb_simulation* r);   ///< Internal function used to call a specific integrator
 void reb_integrator_ias15_clear(struct reb_simulation* r);         ///< Internal function used to call a specific integrator
 void reb_integrator_ias15_alloc(struct reb_simulation* r);         ///< Internal function, alloctes memory for IAS15 
-void reb_integrator_ias15_part2(struct reb_simulation* r);         ///< Internal function used to call a specific integrator
-void reb_integrator_ias15_reset(struct reb_simulation* r);         ///< Internal function used to call a specific integrator
 #endif
--- src/rebound.h
+++ src/rebound.h
@@ -1003,4 +1003,16 @@ struct reb_display_data {
     unsigned int orbit_shader_vertex_count;
 };
 
+
+// Temporary. Function declarations needed by REBOUNDx 
+void reb_integrator_ias15_reset(struct reb_simulation* r);         ///< Internal function used to call a specific integrator
+void reb_integrator_ias15_part2(struct reb_simulation* r);         ///< Internal function used to call a specific integrator
+void reb_integrator_whfast_from_inertial(struct reb_simulation* const r);   ///< Internal function to the appropriate WHFast coordinates from inertial
+void reb_integrator_whfast_to_inertial(struct reb_simulation* const r); ///< Internal function to move back from particular WHFast coordinates to inertial
+void reb_integrator_whfast_reset(struct reb_simulation* r);		///< Internal function used to call a specific integrator
+int reb_integrator_whfast_init(struct reb_simulation* const r);    ///< Internal function to check errors and allocate memory if needed
+void reb_whfast_interaction_step(struct reb_simulation* const r, const double _dt);///< Internal function
+void reb_whfast_jump_step(const struct reb_simulation* const r, const double _dt); ///< Internal function
+void reb_whfast_kepler_step(const struct reb_simulation* const r, const double _dt); ///< Internal function
+void reb_whfast_com_step(const struct reb_simulation* const r, const double _dt); ///< Internal function
 #endif // _MAIN_H
