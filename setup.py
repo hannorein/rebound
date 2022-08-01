b812a3b21c86a41289fe0a3f990400d8863cc8dc
Hanno Rein <hanno@hanno-rein.de>
2022-08-01 21:03:54 +0200

    Updating version to 3.19.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ try:
     ghash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii")
     ghash_arg = "-DGITHASH="+ghash.strip()
 except:
-    ghash_arg = "-DGITHASH=093ab32b0babb5198a2c5dd6b61ed6561094e243" #GITHASHAUTOUPDATE
+    ghash_arg = "-DGITHASH=6d9c8082c903d82d3c7f8766a20f6388cfd61893" #GITHASHAUTOUPDATE
 
 extra_link_args=[]
 if sys.platform == 'darwin':
@@ -64,7 +64,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='rebound',
-    version='3.19.5',
+    version='3.19.6',
     description='An open-source multi-purpose N-body code',
     long_description=long_description,
     long_description_content_type="text/markdown",
