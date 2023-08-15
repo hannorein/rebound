f174c95a6acc9e80ff025cedcf3f8445cc5fa1c0
Tiger Lu <tigerlu@Tigers-MBP-2.stdusr.yale.internal>
2023-08-15 11:19:45 -0400

    violent checkpoint

--- examples/violent/problem.c
+++ examples/violent/problem.c
@@ -41,30 +41,46 @@ int main(int argc, char* argv[]){
     star.r = 0.005;
     reb_add(r, star);
 
+    r->rand_seed = 1;
+
     for (int i = 0; i < 10; i++){
       // Add bodies
-      reb_add_fmt(r, "m r a f Omega inc", 0.005, 0.0005, as[i], fs[i], Omegas[i], (double)i * M_PI / 180.);
+      double loga = reb_random_uniform(r, log10(0.3), log10(10.));
+      reb_add_fmt(r, "m r a f Omega inc", 0.005, 0.0005, pow(10, loga), reb_random_uniform(r, 0., 2.*M_PI), reb_random_uniform(r, 0., 2.*M_PI), (double)i * M_PI / 180.);
     }
 
-    r->dt = 0.059331635924546614;
+    struct reb_particle* sun = &r->particles[0];
+    double min = INFINITY;
+    for (int i = 1; i < 11; i++){
+      struct reb_particle* p = &r->particles[i];
+      struct reb_orbit o = reb_tools_particle_to_orbit(r->G, *p, *sun);
+      if (o.P < min){
+        min = o.P;
+      }
+    }
+    r->dt = min * 0.05;//0.059331635924546614;
 
     r->integrator = REB_INTEGRATOR_TRACE;
-    r->ri_tr.hillfac = 4;            // By default the switching radius is 4 times the hill radius, from Hernandez 2023
-    r->ri_tr.peri = 10;
+    r->ri_tr.vSwitch=1;
+    r->ri_tr.vfac = 30.;
+
+    //r->ri_tr.hillfac = 4;            // By default the switching radius is 4 times the hill radius, from Hernandez 2023
+    //r->ri_tr.peri = 0.3;
     r->visualization = REB_VISUALIZATION_NONE;
     r->heartbeat  = heartbeat;
 
     reb_move_to_com(r);                // This makes sure the planetary systems stays within the computational domain and doesn't drift.
     system("rm -rf energy.txt");
     FILE* f = fopen("energy.txt","w");
-    fprintf(f, "t,E");
+    fprintf(f, "t,E,x0,y0,z0");
     for (int i = 1; i < 11; i++){
-      fprintf(f, ",a%d,e%d,i%d,x%d,y%d,z%d", i, i, i,i,i,i);
+      fprintf(f, ",a%d,e%d,i%d,x%d,y%d,z%d",i,i,i,i,i,i);
     }
     fprintf(f, "\n");
     fclose(f);
 
-    reb_integrate(r, 500*2*M_PI);
+    reb_integrate(r, 1.*2*M_PI);
+    printf("%d %d %d %d %d\n", r->ri_tr.delta_Ks[0], r->ri_tr.delta_Ks[1], r->ri_tr.delta_Ks[2], r->ri_tr.delta_Ks[3], r->ri_tr.delta_Ks[4], r->ri_tr.delta_Ks[5]);
     reb_free_simulation(r);
 }
 
@@ -73,19 +89,20 @@ void heartbeat(struct reb_simulation* r){
     //if (reb_output_check(r, 10.*2.*M_PI)){
     //    reb_output_timing(r, 0);
     //}
-    if (reb_output_check(r, 2.*M_PI)){
+    //if (reb_output_check(r, 2.*M_PI)){
         // Once per 4 days, output the relative energy error to a text file
         FILE* f = fopen("energy.txt","a");
 
         fprintf(f, "%e,%e", r->t, reb_tools_energy(r));
 
         struct reb_particle* sun = &r->particles[0];
+        fprintf(f, ",%e,%e,%e",sun->x,sun->y,sun->z);
         for (int i = 0; i < 10; i++){
           struct reb_particle* p = &r->particles[i+1];
           struct reb_orbit o = reb_tools_particle_to_orbit(r->G, *p, *sun);
-          fprintf(f, ",%e,%e,%e,%e,%e,%e", o.a, o.e, o.inc,p.x,p.y,p.z);
+          fprintf(f, ",%e,%e,%e,%e,%e,%e", o.a, o.e, o.inc,p->x,p->y,p->z);
         }
         fprintf(f, "\n");
         fclose(f);
-    }
+    //}
 }
