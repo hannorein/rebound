80a4dadf132cde308b780a802a9ed4f968d576e3
Hanno Rein <hanno@hanno-rein.de>
2023-08-29 17:59:06 -0400

    problem

--- examples/binary_field_type_reverse/problem.c
+++ examples/binary_field_type_reverse/problem.c
@@ -6,6 +6,7 @@
  * units.
  */
 #include "rebound.h"
+#include "output.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,16 +17,27 @@ int main(int argc, char* argv[]) {
     reb_add_fmt(r, "m a e", 1e-3, 1., 0.1); // Jupiter mass planet
     reb_add_fmt(r, "a e", 1.4, 0.1);        // Massless test particle 
 
-    reb_integrate(r,100.);
+    reb_steps(r,1);
 
+    char* buf = NULL;
+    size_t size = 0;
+    reb_output_binary_to_stream(r, &buf, &size);
+    
+
+
+    struct reb_simulationarchive* sa = malloc(sizeof(struct reb_simulationarchive));
+    reb_read_simulationarchive_from_buffer_with_messages(sa, buf, size, NULL, NULL);
+
+    
     struct reb_simulation* r2 = reb_create_simulation();
+    enum reb_input_binary_messages w =0;
+    reb_create_simulation_from_simulationarchive_with_messages(r2,sa,-1,&w);
 
-    reb_output_binary(r2, "test");
+    reb_close_simulationarchive(sa);
+    free(buf);
+    printf("sizeof(size_t)= %ld\n", sizeof(unsigned int));
 
-    struct reb_simulation* r3 = reb_create_simulation_from_binary("test");
-    reb_diff_simulations(r3, r,1);
-
-//    reb_free_simulation(r);
-  //  reb_free_simulation(r2);
+    reb_free_simulation(r);
+    reb_free_simulation(r2);
 }
 
