5d59a9d6d89709ba2d9f651aa523d8f5dc264cd4
Hanno Rein <hanno@hanno-rein.de>
2023-11-02 10:56:17 -0400

    server

---
+++ src/server.c
@@ -0,0 +1,149 @@
+/* 
+ * tiny.c - a minimal HTTP server
+ *          Dave O'Hallaron, Carnegie Mellon
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "rebound.h"
+
+#define BUFSIZE 1024
+
+extern char **environ; /* the environment */
+
+/*
+ * cerror - returns an error message to the client
+ */
+void cerror(FILE *stream, char *cause, char *errno, 
+        char *shortmsg, char *longmsg) {
+    fprintf(stream, "HTTP/1.1 %s %s\n", errno, shortmsg);
+    fprintf(stream, "Content-type: text/html\n");
+    fprintf(stream, "\n");
+    fprintf(stream, "<html><title>Tiny Error</title>");
+    fprintf(stream, "<body bgcolor=""ffffff"">\n");
+    fprintf(stream, "%s: %s\n", errno, shortmsg);
+    fprintf(stream, "<p>%s: %s\n", longmsg, cause);
+    fprintf(stream, "<hr><em>The Tiny Web server</em>\n");
+}
+
+void* start_server(void* args){
+    reb_sigint = 0;
+    signal(SIGINT, reb_sigint_handler);
+    struct reb_thread_info* thread_info = (struct reb_thread_info*)args;
+
+    /* variables for connection management */
+    int parentfd;          /* parent socket */
+    int childfd;           /* child socket */
+    int portno;            /* port to listen on */
+    unsigned int clientlen;         /* byte size of client's address */
+    struct hostent *hostp; /* client host info */
+    int optval;            /* flag value for setsockopt */
+    struct sockaddr_in serveraddr; /* server's addr */
+    struct sockaddr_in clientaddr; /* client addr */
+
+    /* variables for connection I/O */
+    FILE *stream;          /* stream version of childfd */
+    char buf[BUFSIZE];     /* message buffer */
+    char method[BUFSIZE];  /* request method */
+    char uri[BUFSIZE];     /* request uri */
+    char version[BUFSIZE]; /* request method */
+
+    /* check command line args */
+    portno = 1234;
+
+    /* open socket descriptor */
+    parentfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (parentfd < 0) 
+        reb_exit("ERROR opening socket");
+
+    /* allows us to restart server immediately */
+    optval = 1;
+    setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR, 
+            (const void *)&optval , sizeof(int));
+
+    /* bind port to socket */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)portno);
+    if (bind(parentfd, (struct sockaddr *) &serveraddr, 
+                sizeof(serveraddr)) < 0) 
+        reb_exit("ERROR on binding");
+
+    /* get us ready to accept connection requests */
+    if (listen(parentfd, 5) < 0) /* allow 5 requests to queue up */ 
+        reb_exit("ERROR on listen");
+
+    printf("Listening on port %d...\n",portno);
+    /* 
+     * main loop: wait for a connection request, parse HTTP,
+     * serve requested content, close connection.
+     */
+    clientlen = sizeof(clientaddr);
+    while (1) {
+        /* wait for a connection request */
+        childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
+        if (childfd < 0) 
+            reb_exit("ERROR on accept");
+
+        /* determine who sent the message */
+        hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+                sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        if (hostp == NULL)
+            reb_exit("ERROR on gethostbyaddr");
+
+        /* open the child socket descriptor as a stream */
+        if ((stream = fdopen(childfd, "r+")) == NULL)
+            reb_exit("ERROR on fdopen");
+
+        /* get the HTTP request line */
+        char* request = fgets(buf, BUFSIZE, stream);
+        if (!request){
+            cerror(stream, method, "501", "Not Implemented", "Tiny did not get request");
+            fclose(stream);
+            close(childfd);
+            continue;
+        }
+
+        sscanf(buf, "%s %s %s\n", method, uri, version);
+
+        /* tiny only supports the GET method */
+        if (strcasecmp(method, "GET")) {
+            cerror(stream, method, "501", "Not Implemented", "Tiny does not implement this method");
+            fclose(stream);
+            close(childfd);
+            continue;
+        }
+            
+        printf("URI: %s\n",uri);
+
+        /* read (and ignore) the HTTP headers */
+        fgets(buf, BUFSIZE, stream);
+        //printf("%s", buf);
+        while(strcmp(buf, "\r\n")) {
+            fgets(buf, BUFSIZE, stream);
+            //printf("%s", buf);
+        }
+
+        /* print response header */
+        fprintf(stream, "HTTP/1.1 200 OK\n");
+        fprintf(stream, "Server: Tiny Web Server\n");
+        //fprintf(stream, "Content-length: %d\n", (int)sbuf.st_size);
+        fprintf(stream, "Content-type: text/html\n");
+        fprintf(stream, "\r\n"); 
+        fflush(stream);
+        fprintf(stream, "<html><title>All good</title>");
+        fprintf(stream, "<body bgcolor=""ffffff"">\n");
+        fprintf(stream, "<hr>Hello!\n");
+
+        /* clean up */
+        fclose(stream);
+        close(childfd);
+
+    }
+}
