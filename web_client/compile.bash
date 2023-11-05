bbbda2ef92f6b655b66d85e2241a20b7c3bf467a
Hanno Rein <hanno@hanno-rein.de>
2023-11-05 10:30:55 -0500

    make file client

--- web_client/compile.bash
+++ web_client/compile.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 # To be called from main rebound directory
 source emsdk/emsdk_env.sh
-          
-emcc -O3 -Isrc/ src/*.c web_client/problem.c -DOPENGL=1 -sSTACK_SIZE=655360 -s USE_GLFW=3 -s FULL_ES3=1 -sASYNCIFY -sFETCH -sSINGLE_FILE --shell-file docs/c_examples/shell_rebound.html -o web_client/rebound.html
+make -C web_client
