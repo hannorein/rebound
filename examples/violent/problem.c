f2538c4beee6f2349f741ad155f06948c14f7a34
Tiger Lu <tigerlu@Tigers-MBP-2.stdusr.yale.internal>
2023-11-15 13:07:02 -0500

    redo IAS15 testing, fixed boundary condition

--- examples/violent/problem.c
+++ examples/violent/problem.c
@@ -18,10 +18,11 @@ double tmax = 1e7*2*M_PI;
 int nbodies=3;
 int first_ejected = 999;
 int ind;
+int nparticles;
 
-char title[100] = "trace_evolution_";
-char title_stats[100] = "trace_first_ejected.txt";
-char title_remove[100] = "rm -rf trace_evolution_";
+char title[100] = "violent_ias15/redo_chaotic_ias15_";
+char title_stats[100] = "redo_chaotic_ias15_time_stats";
+char title_remove[100] = "rm -rf violent_ias15/redo_chaotic_ias15_";
 
 int main(int argc, char* argv[]){
 
@@ -46,15 +47,12 @@ int main(int argc, char* argv[]){
     r->rand_seed = ind;
 
     for (int i = 0; i < nbodies; i++){
-      double Om_rand = 0.0;//reb_random_uniform(r, 0, 2 * M_PI);
-      double f_rand = 0.0;//reb_random_uniform(r, 0, 2 * M_PI);
       if (i == 2){
         sma += reb_random_uniform(r, -1e-12, 1e-12);
       }
-      reb_add_fmt(r, "m a e inc Omega f hash", planet_m, sma, 0.05, (double)i * M_PI / 180., Om_rand, f_rand, i+1);
+      reb_add_fmt(r, "m a e inc hash", planet_m, sma, 0.05, (double)i * M_PI / 180., i+1);
       double num = -pow(2., 1./3.) * pow(3., 1./3.) * sma - pow((planet_m / star.m), 1./3.) * delta * sma;
       double denom = -pow(2., 1./3.) * pow(3., 1./3.) + pow((planet_m / star.m), 1./3.) * delta;
-
       sma = num/denom;
     }
 
@@ -69,8 +67,10 @@ int main(int argc, char* argv[]){
         }
     }
 
-    r->integrator = REB_INTEGRATOR_TRACE;
-    r->dt = min * 0.05;//7.108147e-01;//min * 0.010123456;//0.059331635924546614;
+    //r->integrator = REB_INTEGRATOR_TRACE;
+    r->integrator = REB_INTEGRATOR_IAS15;
+    r->ri_ias15.adaptive_mode = 2;
+    //r->dt = min * 0.05;//7.108147e-01;//min * 0.010123456;//0.059331635924546614;
     //r->ri_tr.S_peri = reb_integrator_trace_switch_fdot_peri;
     r->track_energy_offset = 1;
     reb_configure_box(r, 10000., 1, 1, 1);
@@ -78,7 +78,7 @@ int main(int argc, char* argv[]){
     r->heartbeat  = heartbeat;
 
     reb_move_to_com(r);                // This makes sure the planetary systems stays within the computational domain and doesn't drift.
-
+/*
     if (r->heartbeat != NULL){
       system(title_remove);
       FILE* f = fopen(title, "w");
@@ -87,21 +87,21 @@ int main(int argc, char* argv[]){
       fprintf(f, "t,E");
       for (int i = 1; i < nbodies+1; i++){
         //fprintf(f, ",a%d,x%d,y%d,z%d,vx%d,vy%d,vz%d",i,i,i,i,i,i,i);
-        fprintf(f, ",a%d,e%d,i%d",i,i,i);
+        fprintf(f, ",a%d,e%d,i%d,x%d,y%d,z%d",i,i,i,i,i,i);
         //fprintf(f, ",hash%d,d%d,ej%d",i,i,i);
       }
 
       fprintf(f, "\n");
       fclose(f);
     }
-
-    //tmax = 1e7 * 2 * M_PI;//1e6*2*M_PI;//2e6*2*M_PI; //min * 100000;
+*/
     e_init = reb_tools_energy(r);
-    //clock_t begin = clock();
+    nparticles = r->N;
+    clock_t begin = clock();
     reb_integrate(r, tmax);
-    //clock_t end = clock();
-    //double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-/*
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
     int surviving_particles = 0;
     for (unsigned int i = 1; i < nbodies+1; i++){
         struct reb_particle* p = reb_get_particle_by_hash(r, i);
@@ -109,10 +109,10 @@ int main(int argc, char* argv[]){
           surviving_particles++;
         }
     }
-*/
-    //FILE* tf = fopen(title_stats, "a");
-    //printf("%d,%d,%e,%e,%f\n", in, surviving_particles, fabs(reb_tools_energy(r) - e_init)/e_init, time_spent, r->t);
-    //fclose(tf);
+
+    FILE* tf = fopen(title_stats, "a");
+    fprintf(tf, "%d,%d,%e,%e,%f,%d\n", ind, surviving_particles, fabs(reb_tools_energy(r) - e_init)/e_init, time_spent, r->t,r->N);
+    fclose(tf);
 
     //printf("Total: %d\nInit Peri No Flag: %d\nInit Peri Flag: %d\nNo Flags:%d\nFlagged Peri:%d\nFlagged CE:%d\n", r->ri_tr.delta_Ks[0], r->ri_tr.delta_Ks[1], r->ri_tr.delta_Ks[2], r->ri_tr.delta_Ks[3], r->ri_tr.delta_Ks[4], r->ri_tr.delta_Ks[5]);
     reb_free_simulation(r);
@@ -121,12 +121,18 @@ int main(int argc, char* argv[]){
 
 void heartbeat(struct reb_simulation* r){
     // remove particles if needed
+    if (r->N < nparticles){
+      reb_move_to_com(r);
+      nparticles = r->N;
+    }
+
     //if (reb_output_check(r, 10.*2.*M_PI)){
     //    reb_output_timing(r, tmax);
     //}
 
     // Time to first ejection
     // Always track
+    /*
     if (first_ejected == 999){ // ejection has not happened yet
       for (unsigned int i = 1; i < nbodies+1; i++){
           struct reb_particle* p = reb_get_particle_by_hash(r, i);
@@ -144,7 +150,8 @@ void heartbeat(struct reb_simulation* r){
         fclose(fs);
       }
     }
-
+    */
+/*
     if (reb_output_check(r, 10. * 2.*M_PI)){
 
       FILE* f = fopen(title, "a");
@@ -155,14 +162,14 @@ void heartbeat(struct reb_simulation* r){
         struct reb_particle* p = reb_get_particle_by_hash(r, i);
         if (p != NULL){
           struct reb_orbit o = reb_tools_particle_to_orbit(r->G, *p, *sun);
-          fprintf(f, ",%f,%f,%f", o.a, o.e, o.inc);
+          fprintf(f, ",%f,%f,%f,%f,%f,%f", o.a, o.e, o.inc,p->x,p->y,p->z);
         }
         else{
-          fprintf(f, "-1,-1,1e10");
+          fprintf(f, ",-1,-1,-1,1e10,1e10,1e10");
         }
       }
       fprintf(f, "\n");
       fclose(f);
     }
-
+*/
 }
