e407d7aa82ad5368a22b8f298234ef5abffef3e5
Tiger Lu <tigerlu@Tigers-MBP-2.hsd1.ct.comcast.net>
2024-01-04 14:29:57 -0500

    revising kozai files

--- examples/tkozai/problem.c
+++ examples/tkozai/problem.c
@@ -16,15 +16,14 @@
  #include <math.h>
  #include <time.h>
  #include "rebound.h"
- #include "integrator_trace.h"
 
 void heartbeat(struct reb_simulation* r);
-double tmax = 5e7 * 2 * M_PI;
+double tmax = 1e7 * 2 * M_PI;
 clock_t begin;
 clock_t end;
 double e_init;
-char title[100] = "trace_nt";
-char title_remove[100] = "rm -rf trace_nt";
+char title[100] = "ias15_nt";
+char title_remove[100] = "rm -rf ias15_nt";
 
 double obl(struct reb_vec3d v1, struct reb_vec3d v2){
   return acos(reb_vec3d_dot(v1,v2) / (sqrt(reb_vec3d_length_squared(v1)) * sqrt(reb_vec3d_length_squared(v2))));
@@ -34,8 +33,8 @@ int main(int argc, char* argv[]){
     // Setup constants
     struct reb_simulation* sim = reb_simulation_create();
     //sim->dt             = 2*M_PI*1.;     // initial timestep
-    sim->integrator        = REB_INTEGRATOR_TRACE;
-    //sim->ri_ias15.adaptive_mode=2;
+    sim->integrator        = REB_INTEGRATOR_IAS15;
+    sim->ri_ias15.adaptive_mode=2;
     sim->heartbeat        = heartbeat;
 
     // Initial conditions Naoz et al 2016 Figure 16
@@ -53,8 +52,8 @@ int main(int argc, char* argv[]){
 
     struct reb_orbit o = reb_orbit_from_particle(sim->G, sim->particles[1], sim->particles[0]);
     sim->dt = o.P/50.12345;
-    sim->ri_trace.S_peri = reb_integrator_trace_peri_switch_default;
-    sim->ri_trace.peri_distance = 1.;
+    //sim->ri_trace.S_peri = reb_integrator_trace_peri_switch_default;
+    //sim->ri_trace.peri_distance = 1.;
     //sim->ri_trace.pfdot=1000.;
     //sim->ri_bs.eps_rel = 1e-11;            // Relative tolerance
     //sim->ri_bs.eps_abs = 1e-11;            // Absolute tolerance
@@ -80,7 +79,7 @@ int main(int argc, char* argv[]){
     system(title_remove);
     FILE* f = fopen(title,"w");
     //fprintf(f, "t,E,a1,i1,e1,p_ob,a2,i2,e2,pert_ob,mi,theta_p,time_spent\n");
-    fprintf(f, "t,E,a1,i1,e1,a2,i2,e2,mi,time_spent,hj,hk\n");
+    fprintf(f, "t,E,a1,i1,e1,a2,i2,e2,mi,time_spent\n");
     fclose(f);
 
     begin = clock();
@@ -113,36 +112,8 @@ int main(int argc, char* argv[]){
       struct reb_vec3d n2 = o2.hvec;
       double mi = obl(n1,n2);
 
-      reb_integrator_trace_inertial_to_dh(sim);
-      double px0=0., py0=0., pz0=0.;
-      double mtot = sim->particles[0].m;
-
-      for (unsigned int i=0;i<sim->N;i++){
-          px0 += sim->particles[i].vx*sim->particles[i].m;
-          py0 += sim->particles[i].vy*sim->particles[i].m;
-          pz0 += sim->particles[i].vz*sim->particles[i].m;
-          mtot += sim->particles[i].m;
-      }
-
-      int j = 1;
-      const double pjx = (sim->particles[j].vx * sim->particles[j].m) - (sim->particles[j].m / mtot) * px0;
-      const double pjy = (sim->particles[j].vy * sim->particles[j].m) - (sim->particles[j].m / mtot) * py0;
-      const double pjz = (sim->particles[j].vz * sim->particles[j].m) - (sim->particles[j].m / mtot) * pz0;
-      const double p02 = px0*px0 + py0*py0+pz0*pz0;
-      const double pj2 = pjx*pjx + pjy * pjy + pjz * pjz;
-      const double hj = p02 / (2. * sim->particles[0].m);
-
-      // Kepler
-      const double dx = sim->particles[j].x - sim->particles[0].x;
-      const double dy = sim->particles[j].y - sim->particles[0].y;
-      const double dz = sim->particles[j].z - sim->particles[0].z;
-      const double d2 = dx*dx + dy*dy + dz*dz;
-      const double d = sqrt(d2);
-      const double hk = pj2 / (2 * sim->particles[j].m) - ((sim->G * sim->particles[0].m * sim->particles[j].m) / d);
-
-      fprintf(f, "%f,%e,%f,%f,%f,%f,%f,%f,%f,%f,%e,%e\n", sim->t,fabs((reb_simulation_energy(sim) - e_init) / e_init),a1,i1,e1,a2,i2,e2,mi,time_spent,hj,hk); // print spins and orbits
+      fprintf(f, "%f,%e,%f,%f,%f,%f,%f,%f,%f,%f\n", sim->t,fabs((reb_simulation_energy(sim) - e_init) / e_init),a1,i1,e1,a2,i2,e2,mi,time_spent); // print spins and orbits
       fclose(f);
-      reb_integrator_trace_dh_to_inertial(sim);
       reb_simulation_integrate(sim, sim->t + 1e4*2*M_PI);
       }
     end = clock();
