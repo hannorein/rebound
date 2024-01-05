a0e9a42adfbe598ff523db8de18d138b3cb7a6c9
Hanno Rein <hanno@hanno-rein.de>
2024-01-05 10:58:07 -0500

    Updating version to 4.0.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ try:
     ghash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii")
     ghash_arg = "-DGITHASH="+ghash.strip()
 except:
-    ghash_arg = "-DGITHASH=8532f9cf49eff9758bf6ed829201a2308e3b2100" #GITHASHAUTOUPDATE
+    ghash_arg = "-DGITHASH=fb5088af9c5867f720815c4ac42b2d674f6a311b" #GITHASHAUTOUPDATE
 
 extra_link_args=[]
 if sys.platform == 'darwin':
@@ -83,7 +83,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='rebound',
-    version='4.0.2',
+    version='4.0.3',
     description='An open-source multi-purpose N-body code',
     long_description=long_description,
     long_description_content_type="text/markdown",
