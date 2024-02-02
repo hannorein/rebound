329a64139294e7be5b27de5087123785b21c99e4
Hanno Rein <hanno@hanno-rein.de>
2024-02-02 17:42:18 -0500

    basic unit tests started

--- rebound/tests/test_trace_basic.py
+++ rebound/tests/test_trace_basic.py
@@ -3,19 +3,38 @@ import unittest
 import numpy as np
 
 
-class TestIntegratorTraceHarmonic(unittest.TestCase):
+class TestIntegratorTraceBasic(unittest.TestCase):
    
-    def test_trace_harmonic_with_nbody(self):
+    def test_trace_encounter_condition(self):
         sim = rebound.Simulation()
         sim.add(m=1)
-        sim.add(m=1e-3,a=1);
-        sim.add(m=1e-3,a=1.1);
+        sim.add(m=1e-3,a=1)
+        sim.add(m=1e-3,a=1.1,f=0.25) # First step will be rejected.
+        sim.add(m=1e-3,a=3.1)
         sim.integrator = "TRACE"
+        sim.dt = 0.14 
 
-        sim.step()
+        for k in range(3):
+            sim.step()
+            self.assertEqual(sim.ri_trace._current_C,0)
+            self.assertEqual(sim.ri_trace._encounter_N,3)
+            self.assertEqual(sim.ri_trace._encounter_N_active,3)
+            for i in range(sim.N):
+                for j in range(sim.N):
+                    if i==1 and j==2: 
+                        self.assertEqual(sim.ri_trace._current_Ks[i*sim.N+j],1)
+                    else:
+                        self.assertEqual(sim.ri_trace._current_Ks[i*sim.N+j],0)
         
-        self.assertEqual(sim.ri_trace._current_C,0)
-        self.assertEqual(sim.ri_trace._current_Ks[1],0)
+        # Change condition. No more encounters.
+        sim.ri_trace.r_crit_hill = 0.1
+        for k in range(3):
+            sim.step()
+            self.assertEqual(sim.ri_trace._current_C,0)
+            self.assertEqual(sim.ri_trace._encounter_N,1)
+            for i in range(sim.N):
+                for j in range(sim.N):
+                    self.assertEqual(sim.ri_trace._current_Ks[i*sim.N+j],0)
 
 
 if __name__ == "__main__":
