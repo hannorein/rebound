497628a36616d3c0cd5ba1ef406736b3a5018110
Tiger Lu <tigerlu@Tigers-MBP-2.stdusr.yale.internal>
2024-02-21 14:46:40 -0500

    trace full ias15

--- examples/violent/problem.c
+++ examples/violent/problem.c
@@ -20,8 +20,8 @@ int first_ejected = 999;
 int ind;
 
 char title[100] = "timestamps/ias15_stats_";
-char title_stats[100] = "221_trace_stats";//"merc_timestamps/mercurius_first_ejection";
-char element_stats[100] = "221_trace_element_stats";//"merc_timestamps/mercurius_first_ejection";
+char title_stats[100] = "221_trace_fullias15_stats";//"merc_timestamps/mercurius_first_ejection";
+char element_stats[100] = "221_trace_fullias15_element_stats";//"merc_timestamps/mercurius_first_ejection";
 char title_remove[100] = "rm -rf timestamps/ias15_stats_";
 
 int main(int argc, char* argv[]){
@@ -75,12 +75,12 @@ int main(int argc, char* argv[]){
     r->ri_trace.peri_crit_distance = 0.25 * final_a;
     //r->ri_trace.S_peri = reb_integrator_trace_switch_peri_distance;
     r->dt = final_ts;
-    //r->ri_ias15.adaptive_mode = 2;
+    r->ri_ias15.adaptive_mode = 2;
     r->exit_max_distance = 1e4;
     r->collision = REB_COLLISION_DIRECT;
     r->collision_resolve = reb_collision_resolve_merge;
     r->track_energy_offset = 1;
-    r->ri_trace.peri_mode = REB_TRACE_PERI_PARTIAL_BS;
+    r->ri_trace.peri_mode = REB_TRACE_PERI_FULL_IAS15;
     //r->heartbeat  = heartbeat;               // This makes sure the planetary systems stays within the computational domain and doesn't drift.
 
     if (r->heartbeat != NULL){
