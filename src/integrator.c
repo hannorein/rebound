adce8779bf880f3fa5665aabf247c6a49e771754
Hanno Rein <hanno@hanno-rein.de>
2025-07-02 06:55:07 -0400

    use enum instead of integers

--- src/integrator.c
+++ src/integrator.c
@@ -234,7 +234,7 @@ void reb_simulation_update_acceleration(struct reb_simulation* r){
     if (r->N_var){
         reb_calculate_acceleration_var(r);
     }
-    if (r->additional_forces  && (r->integrator != REB_INTEGRATOR_MERCURIUS || r->ri_mercurius.mode==0) && (r->integrator != REB_INTEGRATOR_TRACE || r->ri_trace.mode==0 || r->ri_trace.mode==3)){
+    if (r->additional_forces  && (r->integrator != REB_INTEGRATOR_MERCURIUS || r->ri_mercurius.mode==0) && (r->integrator != REB_INTEGRATOR_TRACE || r->ri_trace.mode==REB_TRACE_MODE_INTERACTION || r->ri_trace.mode==REB_TRACE_MODE_FULL)){
         // For Mercurius:
         // Additional forces are only calculated in the kick step, not during close encounter
         if (r->integrator==REB_INTEGRATOR_MERCURIUS){
