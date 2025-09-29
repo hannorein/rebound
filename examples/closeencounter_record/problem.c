8dda93c3adf4db9c80e35cc7c9964c84944ff096
Hanno Rein <hanno@hanno-rein.de>
2025-09-29 08:59:35 -0400

    updated enum in examples

--- examples/closeencounter_record/problem.c
+++ examples/closeencounter_record/problem.c
@@ -16,7 +16,7 @@
 #include "rebound.h"
 
 // Define our own collision resolve function, which will only record collisions but not change any of the particles.        
-int collision_record_only(struct reb_simulation* const r, struct reb_collision c){
+enum REB_COLLISION_RESOLVE_OUTCOME collision_record_only(struct reb_simulation* const r, struct reb_collision c){
     double delta_t = 2.*M_PI;     
     struct reb_particle* particles = r->particles;
     const double t = r->t;
@@ -33,7 +33,7 @@ int collision_record_only(struct reb_simulation* const r, struct reb_collision c
         fprintf(of, "\n");
         fclose(of);                        // close file
     }
-    return 0;
+    return REB_COLLISION_RESOLVE_OUTCOME_REMOVE_NONE;
 }
 
 
