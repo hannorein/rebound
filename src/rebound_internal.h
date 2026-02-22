6b92118e717e28d82c6f40c3d016aabd47c01fd1
Hanno Rein <hanno@hanno-rein.de>
2026-02-21 19:01:48 -0500

    internal

---
+++ src/rebound_internal.h
@@ -0,0 +1,28 @@
+/**
+ * @file    rebound_internal.h
+ * @brief   Various functions used internally and implemented in rebound.c
+ * @author  Hanno Rein <hanno@hanno-rein.de>
+ * 
+ * @section     LICENSE
+ * Copyright (c) 2026 Hanno Rein
+ *
+ * This file is part of rebound.
+ *
+ * rebound is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rebound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _REBOUND_INTERNAL_H
+#define // _REBOUND_INTERNAL_H
+#endif // _REBOUND_INTERNAL_H
