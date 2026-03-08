e1fa2e57321b188893e8256e8b51b4a9f2f41957
Hanno Rein <hanno@hanno-rein.de>
2026-03-08 01:36:49 +0000

    asm exp

--- src/integrator_whfast512.s
+++ src/integrator_whfast512.s
@@ -1,10 +1,32 @@
 # file: add_func.s
+.section .text
 .globl gravity_prefactor_avx512
 
+
 gravity_prefactor_avx512:
-    movq %rdi, %rax       # Move the first argument (n1) into the return register (rax)
-    addq %rsi, %rax       # Add the second argument (n2) to rax
-    ret                   # Return to the caller
+    # Input:  zmm0=m, zmm1=dx, zmm2=dy, zmm3=dz
+    
+    # 1. r2 = dx*dx
+    vmulpd      %zmm1, %zmm1, %zmm1     
+    
+    # 2. r2 = (dy*dy) + r2
+    # vfmadd231pd: dest = (src1 * src2) + dest
+    vfmadd231pd %zmm2, %zmm2, %zmm1      
+    
+    # 3. r2 = (dz*dz) + r2
+    vfmadd231pd %zmm3, %zmm3, %zmm1      
+    
+    # 4. r = sqrt(r2)
+    vsqrtpd     %zmm1, %zmm2             
+    
+    # 5. r3 = r * r2
+    vmulpd      %zmm1, %zmm2, %zmm2      
+    
+    # 6. return m / r3
+    # Result must be in zmm0
+    vdivpd      %zmm2, %zmm0, %zmm0      
+
+    ret
 
 //static __m512d inline gravity_prefactor_avx512( __m512d m, __m512d dx, __m512d dy, __m512d dz) {
 //    __m512d r2 = _mm512_mul_pd(dx, dx);
