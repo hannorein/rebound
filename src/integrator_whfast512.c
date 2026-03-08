8ba3ac60ec3b391fb61e90e6fbab30d755c2df25
Hanno Rein <hanno@hanno-rein.de>
2026-03-08 17:30:56 +0000

    mid

--- src/integrator_whfast512.c
+++ src/integrator_whfast512.c
@@ -503,43 +503,44 @@ extern __m512d gravity_prefactor_avx512( __m512d m, __m512d dx, __m512d dy, __m5
 // ##################################################################################################
 // ##################################################################################################
 
-void gr_potential( __m512d x_j, __m512d y_j, __m512d z_j, 
+extern void gr_potential( __m512d x_j, __m512d y_j, __m512d z_j, 
         __m512d gr_prefac, __m512d gr_prefac2, __mmask8 mask, 
-        __m512d* hvx, __m512d* hvy, __m512d* hvz,__m512d mdt){
-    __m512d r2 = _mm512_mul_pd(x_j, x_j);
-    r2 = _mm512_fmadd_pd(y_j, y_j, r2);
-    r2 = _mm512_fmadd_pd(z_j, z_j, r2);
-    const __m512d r4 = _mm512_mul_pd(r2, r2);
-    __m512d prefac = _mm512_div_pd(gr_prefac,r4);
-    __m512d dvx = _mm512_mul_pd(prefac, x_j); 
-    __m512d dvy = _mm512_mul_pd(prefac, y_j); 
-    __m512d dvz = _mm512_mul_pd(prefac, z_j); 
-    *hvx  = dvx;
-    *hvy  = dvy;
-    *hvz  = dvz;
+        __m512d* hvx, __m512d* hvy, __m512d* hvz,__m512d mdt);
 
-    // Calculate back reaction onto star and apply them to planets (heliocentric) 
-    dvx = _mm512_maskz_mul_pd(mask, gr_prefac2, dvx); 
-    dvy = _mm512_maskz_mul_pd(mask, gr_prefac2, dvy); 
-    dvz = _mm512_maskz_mul_pd(mask, gr_prefac2, dvz); 
-
-    double sum_x = _mm512_reduce_add_pd(dvx);
-    double sum_y = _mm512_reduce_add_pd(dvy);
-    double sum_z = _mm512_reduce_add_pd(dvz);
-    *hvx = _mm512_maskz_sub_pd(mask, *hvx, _mm512_set1_pd(sum_x));
-    *hvy = _mm512_maskz_sub_pd(mask, *hvy, _mm512_set1_pd(sum_y));
-    *hvz = _mm512_maskz_sub_pd(mask, *hvz, _mm512_set1_pd(sum_z));
-
-    // Jacobi additions:
-    // Need to add stellar term. Easy: already in heliocentric coordinates.
-    // TODO: Should put a mask on particle 1 as +/- cancels.
-    const __m512d r1 = _mm512_sqrt_pd(r2);
-    const __m512d r3 = _mm512_mul_pd(r1, r2);
-    __m512d prefact = _mm512_div_pd(mdt,r3); // Note: using m0, m0, m0, ... for mass here
-    *hvx = _mm512_maskz_fnmadd_pd(mask, prefact, x_j, *hvx); 
-    *hvy = _mm512_maskz_fnmadd_pd(mask, prefact, y_j, *hvy); 
-    *hvz = _mm512_maskz_fnmadd_pd(mask, prefact, z_j, *hvz); 
-}
+//    __m512d r2 = _mm512_mul_pd(x_j, x_j);
+//    r2 = _mm512_fmadd_pd(y_j, y_j, r2);
+//    r2 = _mm512_fmadd_pd(z_j, z_j, r2);
+//    const __m512d r4 = _mm512_mul_pd(r2, r2);
+//    __m512d prefac = _mm512_div_pd(gr_prefac,r4);
+//    __m512d dvx = _mm512_mul_pd(prefac, x_j); 
+//    __m512d dvy = _mm512_mul_pd(prefac, y_j); 
+//    __m512d dvz = _mm512_mul_pd(prefac, z_j); 
+//    *hvx  = dvx;
+//    *hvy  = dvy;
+//    *hvz  = dvz;
+//
+//    // Calculate back reaction onto star and apply them to planets (heliocentric) 
+//    dvx = _mm512_maskz_mul_pd(mask, gr_prefac2, dvx); 
+//    dvy = _mm512_maskz_mul_pd(mask, gr_prefac2, dvy); 
+//    dvz = _mm512_maskz_mul_pd(mask, gr_prefac2, dvz); 
+//
+//    double sum_x = _mm512_reduce_add_pd(dvx);
+//    double sum_y = _mm512_reduce_add_pd(dvy);
+//    double sum_z = _mm512_reduce_add_pd(dvz);
+//    *hvx = _mm512_maskz_sub_pd(mask, *hvx, _mm512_set1_pd(sum_x));
+//    *hvy = _mm512_maskz_sub_pd(mask, *hvy, _mm512_set1_pd(sum_y));
+//    *hvz = _mm512_maskz_sub_pd(mask, *hvz, _mm512_set1_pd(sum_z));
+//
+//    // Jacobi additions:
+//    // Need to add stellar term. Easy: already in heliocentric coordinates.
+//    // TODO: Should put a mask on particle 1 as +/- cancels.
+//    const __m512d r1 = _mm512_sqrt_pd(r2);
+//    const __m512d r3 = _mm512_mul_pd(r1, r2);
+//    __m512d prefact = _mm512_div_pd(mdt,r3); // Note: using m0, m0, m0, ... for mass here
+//    *hvx = _mm512_maskz_fnmadd_pd(mask, prefact, x_j, *hvx); 
+//    *hvy = _mm512_maskz_fnmadd_pd(mask, prefact, y_j, *hvy); 
+//    *hvz = _mm512_maskz_fnmadd_pd(mask, prefact, z_j, *hvz); 
+//}
 
 
 
--- src/integrator_whfast512.s
+++ src/integrator_whfast512.s
@@ -4,11 +4,40 @@
     .align 64
 .one: 
     .double 1.0
+.t2: 
+    .double 2.0
+    .double 2.0
+    .double 2.0
+    .double 2.0
+    .double 2.0
+    .double 2.0
+    .double 2.0
+    .double 2.0
+.t3: 
+    .double 3.0
+    .double 3.0
+    .double 3.0
+    .double 3.0
+    .double 3.0
+    .double 3.0
+    .double 3.0
+    .double 3.0
+.t5: 
+    .double 5.0
+    .double 5.0
+    .double 5.0
+    .double 5.0
+    .double 5.0
+    .double 5.0
+    .double 5.0
+    .double 5.0
+
 
 
 .section .text
 .globl gravity_prefactor_avx512_one
 .globl gravity_prefactor_avx512
+.globl gr_potential
 
 
 gravity_prefactor_avx512_one:
@@ -16,17 +45,15 @@ gravity_prefactor_avx512_one:
     
     vmulpd      %zmm0, %zmm0, %zmm0     
     vfmadd231pd %zmm1, %zmm1, %zmm0      
-    vfmadd231pd %zmm2, %zmm2, %zmm0      
-    # zmm0 is now r^2
+    vfmadd231pd %zmm2, %zmm2, %zmm0     # zmm0 is now r^2
     
     vsqrtpd     %zmm0, %zmm1             
-    vmulpd      %zmm0, %zmm1, %zmm0      # zmm0 is r^3
+    vmulpd      %zmm0, %zmm1, %zmm0     # zmm0 is r^3
    
-    vbroadcastsd .one(%rip), %zmm1       # Todo: keep 1 in a register at all times 
+    vbroadcastsd .one(%rip), %zmm1      # Todo: keep 1 in a register at all times 
     vdivpd      %zmm0, %zmm1, %zmm0      
     
-    # return 1 / r^3 in zmm0
-    ret
+    ret                                 # return 1 / r^3 in zmm0
     
 
 gravity_prefactor_avx512:
@@ -34,13 +61,72 @@ gravity_prefactor_avx512:
     
     vmulpd      %zmm1, %zmm1, %zmm1     
     vfmadd231pd %zmm2, %zmm2, %zmm1      
-    vfmadd231pd %zmm3, %zmm3, %zmm1      
-    # zmm1 is now r^2
+    vfmadd231pd %zmm3, %zmm3, %zmm1     # zmm1 is now r^2
     
     vsqrtpd     %zmm1, %zmm2             
     vmulpd      %zmm1, %zmm2, %zmm2      
     vdivpd      %zmm2, %zmm0, %zmm0      
     
-    # return m / r^3 in zmm0
-    ret
+    ret                                 # return m / r^3 in zmm0
 
+
+.macro REDUCE_ADD_AND_BROADCAST reg, temp_reg
+    vshuff64x2 $0x4E, \reg, \reg, \temp_reg         # 01234567 -> 45670123
+    vaddpd     \reg, \temp_reg, \temp_reg    
+    vshufpd    $0x55, \temp_reg, \temp_reg, \reg    # 01234567 -> 10325476
+    vaddpd     \reg, \temp_reg, \temp_reg
+    vpermpd    $0x4E, \temp_reg, \reg               # 01234567 -> 23016745
+    vaddpd     \reg, \temp_reg, \reg
+.endm
+        
+
+gr_potential:
+    # Input:    zmm0=x_j, zmm1=y_j, zmm2=z_j
+    #           zmm3 = gr_prefac, zmm4 = gr_prefac2
+    #           zmm5 = mdt
+    #           edi = mask
+    #           rsi=&hvx , rdx=&hvy, rcx=&hvz 
+
+	vmulpd	%zmm0, %zmm0, %zmm6
+	vfmadd231pd	%zmm1, %zmm1, %zmm6
+	vfmadd231pd	%zmm2, %zmm2, %zmm6     # zmm6 is r^2
+	
+    vsqrtpd	%zmm6, %zmm18
+	vmulpd	%zmm6, %zmm18, %zmm18
+	vdivpd	%zmm18, %zmm5, %zmm8{%k1}{z}  # zmm8 is  m0*dt/r^3
+
+	vmulpd	%zmm6, %zmm6, %zmm7         # zmm7 is r^4
+	vdivpd	%zmm7, %zmm3, %zmm9{%k1}{z}         
+
+	vmulpd	%zmm9, %zmm0, %zmm15        # is x_j*gr_prefac/r^4 
+	vmulpd	%zmm9, %zmm1, %zmm16
+	vmulpd	%zmm9, %zmm2, %zmm17
+
+    kmovw	%edi, %k1
+	vmulpd	%zmm15, %zmm4, %zmm10{%k1}{z}
+	vmulpd	%zmm16, %zmm4, %zmm11{%k1}{z}
+	vmulpd	%zmm17, %zmm4, %zmm12{%k1}{z}
+
+    REDUCE_ADD_AND_BROADCAST %zmm10, %zmm18   # sum_x
+    REDUCE_ADD_AND_BROADCAST %zmm11, %zmm18
+    REDUCE_ADD_AND_BROADCAST %zmm12, %zmm18
+
+	vsubpd	%zmm10, %zmm15, %zmm10      # hvx = hvx - sum_x
+	vsubpd	%zmm11, %zmm16, %zmm11
+	vsubpd	%zmm12, %zmm17, %zmm12
+
+    vbroadcastsd .t2(%rip), %zmm2
+    vbroadcastsd .t3(%rip), %zmm3
+    vbroadcastsd .t5(%rip), %zmm5
+	vfnmadd213pd	%zmm2, %zmm3, %zmm5    # zmm5 = -(2*5 +3)
+int3
+    # 3                 
+    # zmm0 = zmm10 - zmm8 * %zmm0 
+	vfnmadd213pd	%zmm10, %zmm8, %zmm0    # hvx = hvx - x_j*prefact
+	vmovapd	%zmm0, (%rsi)
+	vfnmadd213pd	%zmm11, %zmm8, %zmm1
+	vmovapd	%zmm1, (%rdx)
+	vfnmadd213pd	%zmm12, %zmm8, %zmm2
+	vmovapd	%zmm2, (%rcx)
+	
+    ret
