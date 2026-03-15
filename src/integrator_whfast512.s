1541e30712e2543f9817d8b0b580b91e443b63ee
Hanno Rein <hanno@hanno-rein.de>
2026-03-15 15:35:07 +0000

    header delet

--- src/integrator_whfast512.s
+++ src/integrator_whfast512.s
@@ -1,11 +1,67 @@
 # file: integrator_whfast512.s
-.include "header.s"
+
+#P512 Structure offsets
+.set P512_M, 0
+.set P512_DT, 64
+.set P512_GR_PREFAC, 128
+.set P512_GR_PREFAC2, 192
+.set P512_m, 320
+.set P512_X, 384
+.set P512_Y, 448
+.set P512_Z, 512
+.set P512_VX, 576
+.set P512_VY, 640
+.set P512_VZ, 704
+.set P512_MAT8_INERTIAL_TO_JACOBI, 768
+.set P512_MAT8_JACOBI_TO_HELIOCENTRIC, 1280
+.set P512_MAT8_INERTIAL_TO_JACOBI_T, 2304
+.set P512_M0, 2816
+.set P512_MASK, 2880
+
+#####################################
+# Register alias
+#####################################
+
+# Only used in Interaction step:
+.set HVX, %zmm13
+.set HVY, %zmm14
+.set HVZ, %zmm15
+.set HVXC, %zmm16
+.set HVYC, %zmm17
+.set HVZC, %zmm18
+.set HX, %zmm19
+.set HY, %zmm20
+.set HZ, %zmm21
+
+# Only used in Kepler step:
+.set R, %zmm13
+.set RI, %zmm14
+.set ZETA, %zmm15
+.set ETA, %zmm16
+.set XX, %zmm17
+.set GS0, %zmm18
+.set GS1, %zmm0     # Note: reusing register zmm0
+.set GS2, %zmm19
+.set GS3, %zmm20
+.set BETA, %zmm21
+
+# Common register use
+.set X, %zmm22
+.set Y, %zmm23
+.set Z, %zmm24
+.set VX, %zmm25
+.set VY, %zmm26
+.set VZ, %zmm27
+.set ONE, %zmm28
+.set DT, %zmm29
+.set HALF_MASK, %zmm30
+.set M, %zmm31
 
 .section .text
 .globl block1_gr
 .globl block1_nogr
 
-reb_whfast512_init_registers:
+.macro reb_whfast512_init_registers
     kmovw           P512_MASK(%rdi), %k1
     vmovapd         P512_DT(%rdi), DT
     vmovapd         P512_M(%rdi), M
@@ -19,9 +75,7 @@ reb_whfast512_init_registers:
     vmovapd     P512_VX(%rdi), VX
     vmovapd     P512_VY(%rdi), VY
     vmovapd     P512_VZ(%rdi), VZ
-    
-    ret
-
+.endm  
 
 .macro gravity_prefactor multiplier=ONE
     # Input:  zmm0=dx, zmm1=dy, zmm2=dy
@@ -198,7 +252,7 @@ reb_whfast512_init_registers:
 #reb_whfast512_kepler_step:
 #    # Need to init registers here when not called after interaction step.
 #    # This will be required for synchronizing.  
-#    call reb_whfast512_init_registers
+#    reb_whfast512_init_registers
 
 .macro reb_whfast512_kepler_step_noinit
 
@@ -279,7 +333,7 @@ reb_whfast512_init_registers:
     #           rsi = Number of steps (counting down)
 
     # Load Constant
-    call reb_whfast512_init_registers
+    reb_whfast512_init_registers
     # Allocate space on stack for matrix multiplications
     subq    $192, %rsp
 
