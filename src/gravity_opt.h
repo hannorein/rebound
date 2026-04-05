98893b0adbc07d7aa6790352afcf98acc85a738a
Ryan Thurber <thurberrt@hotmail.com>
2026-04-05 10:43:38 -0700

    add misssing semicolon

--- src/gravity_opt.h
+++ src/gravity_opt.h
@@ -4,6 +4,6 @@
 #include "rebound.h"
 
 void launch_gravity_basic_naive(int N_real, int N_active, double G, double softening2, unsigned int gravity_ignore_terms,
-                                    reb_vec6d *gb, reb_particle *particles)
+                                    reb_vec6d *gb, reb_particle *particles);
 
 #endif // REBOUND_SRC_GRAVITY_OPT_H
