8e4034ca3ba1242223e97c6514d38968245dccdf
Hanno Rein <hanno@hanno-rein.de>
2026-04-19 08:03:43 -0400

    mend

--- rebound/tests/test_simulationarchive.py
+++ rebound/tests/test_simulationarchive.py
@@ -565,7 +565,7 @@ class TestSimulationarchiveMercurius(unittest.TestCase):
             warnings.simplefilter("always")
             # Will warn that archive is corrupted
             sim.integrate(7001)
-            self.assertEqual(1, len(w))
+            self.assertGreater(len(w),0)
         sa = rebound.Simulationarchive("simulationarchive.bin")
         self.assertEqual(sa.nblobs, 8)
         self.assertAlmostEqual(sa[-1].t, 7000, places=0)
@@ -602,7 +602,7 @@ class TestSimulationarchiveMercurius(unittest.TestCase):
             warnings.simplefilter("always")
             # Will warn that archive is corrupted
             sim.integrate(7001)
-            self.assertEqual(2, len(w))
+            self.assertGreater(len(w),0)
         sa = rebound.Simulationarchive("simulationarchive.bin")
         self.assertEqual(sa.nblobs, 8)
         self.assertAlmostEqual(sa[-1].t, 7000, places=0)
