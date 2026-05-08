e6c2557027f61af1665a4c4737e02e7a5985432a
Hanno Rein <hanno@hanno-rein.de>
2026-05-08 19:43:19 -0400

    readded warning

--- rebound/simulationarchive.py
+++ rebound/simulationarchive.py
@@ -138,9 +138,8 @@ class Simulationarchive(Structure):
         if self.process_warnings:
             sim.process_messages()
 
-        #TODO: Reimplement
-        #if sim.ri_eos.is_synchronized==0 or sim.ri_mercurius.is_synchronized==0 or sim.ri_whfast.is_synchronized==0 or sim.ri_mercurius.is_synchronized==0:
-        #    warnings.warn("The simulation might not be synchronized. You can manually synchronize it by calling sim.synchronize().", RuntimeWarning)
+        if sim.is_synchronized==0:
+            warnings.warn("The simulation might not be synchronized. You can manually synchronize it by calling sim.synchronize().", RuntimeWarning)
 
         return sim
     
