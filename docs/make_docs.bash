051eaac3e103dc393067ba782cc771c3417bdd9b
Hanno Rein <hanno@hanno-rein.de>
2026-05-14 19:19:25 -0400

    doc script

--- docs/make_docs.bash
+++ docs/make_docs.bash
@@ -27,7 +27,6 @@ echo "-----------------"
 python docs/make_c_examples.py
 
 
-
 ## UPDATE START
 mkdocs build  
 ../emsdk/emsdk activate latest
@@ -41,28 +40,27 @@ mkdir -p site/emscripten_c_examples/
 
 for dir in examples/*/
 do
-    echo "Working on $dir ..."
     mpi_enabled=$(cat $dir/Makefile | grep -c "export MPI=1")
     openmp_enabled=$(cat $dir/Makefile | grep -c "export OPENMP=1")
     server_used=$(cat $dir/problem.c | grep -c "reb_simulation_start_server")
     if [ $mpi_enabled -eq 0 ] && [ $openmp_enabled -eq 0 ]; then
         mkdir -p site/emscripten_c_$dir/
-        echo "Compiling... "
+        echo "Compiling $dir ..." 
         if [ $server_used -eq 0 ]; then
             emcc -O$OPTIMI -Isrc/ src/*.c $dir/problem.c -DSERVERHIDEWARNING -sSTACK_SIZE=655360 -s -sASYNCIFY -sALLOW_MEMORY_GROWTH -sEXPORTED_RUNTIME_METHODS="callMain" --shell-file web_client/shell_rebound_console.html -o site/emscripten_c_$dir/index.html || exit 1
         else
             emcc -O$OPTIMI -Isrc/ src/*.c $dir/problem.c -DSERVERHIDEWARNING -DOPENGL=1 -sSTACK_SIZE=655360 -s USE_GLFW=3 -s FULL_ES3=1 -sASYNCIFY -sALLOW_MEMORY_GROWTH -sEXPORTED_RUNTIME_METHODS="callMain" --shell-file web_client/shell_rebound_webgl.html -o site/emscripten_c_$dir/index.html || exit 1
         fi
-        echo "Output written to site/emscripten_c_$dir/index.html" 
     else
-        echo "Skipping."
+        echo "Skipping $dir."
     fi
     echo ""
 
 done
 
 ## UPDATE DONE
-echo "Documentation updated. Releasing lock."
+echo "Documentation updated. Releasing lock"
+echo "-------------------------------------"
 echo "$repository_head" > documentation_head.txt
 rm documentation_lock.txt
 
